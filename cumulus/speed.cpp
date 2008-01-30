dcc74a6f65a81751613a2f3caa1f969475459ab3
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2008-01-30 19:50:53 +0000

    portage to Qt 4.3
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@2324 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/speed.cpp
@@ -0,0 +1,375 @@
+/***************************************************************************
+                          speed.cpp  -  description
+                             -------------------
+    begin                : Sat Jul 20 2002
+    copyright            : (C) 2002 by Andre Somers, 2007 Axel Pauli
+    email                : andre@kflog.org, axel@kflog.org
+
+    This file is part of Cumulus
+
+    $Id$
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <cmath>
+#include <QByteArray>
+
+#include "speed.h"
+
+// initialize static values
+Speed::speedUnit Speed::_horizontalUnit=kilometersPerHour;
+Speed::speedUnit Speed::_verticalUnit=metersPerSecond;
+
+Speed::Speed()
+{
+    _speed=0;
+    _isValid=false;
+    changed();
+}
+
+
+Speed::Speed(double Mps)
+{
+    _speed=Mps;
+    _isValid=true;
+    changed();
+}
+
+
+Speed::~Speed()
+{}
+
+
+void Speed::setVerticalUnit(speedUnit unit)
+{
+    _verticalUnit=unit;
+}
+
+
+void Speed::setHorizontalUnit(speedUnit unit)
+{
+    _horizontalUnit=unit;
+}
+
+
+Speed::speedUnit Speed::getVerticalUnit()
+{
+    return _verticalUnit;
+}
+
+
+Speed::speedUnit Speed::getHorizontalUnit()
+{
+    return _horizontalUnit;
+}
+
+
+/** Set speed in meters per second. */
+void Speed::setMps(double speed)
+{
+    _speed=speed;
+    _isValid=true;
+    changed();
+}
+
+
+/** Set speed in Kilometers per hour */
+void Speed::setKph(double speed)
+{
+    _speed=speed/toKph;
+    _isValid=true;
+    changed();
+}
+
+
+/** Set speed in knots */
+void Speed::setKnot(double speed)
+{
+    _speed=speed/toKnot;
+    _isValid=true;
+    changed();
+}
+
+
+/** Set speed in Nautical miles per hour */
+void Speed::setMph(double speed)
+{
+    _speed=speed/toMph;
+    _isValid=true;
+    changed();
+}
+
+
+/** Set speed in feet per minute */
+void Speed::setFpm(double speed)
+{
+    _speed=speed/toFpm;
+    _isValid=true;
+    changed();
+}
+
+
+/** Get speed in Meters per Second */
+double Speed::getMps() const
+{
+    return (_speed);
+}
+
+
+/** Get speed in Kilometers per hour */
+double Speed::getKph() const
+{
+    return (_speed*toKph);
+}
+
+
+/** Get speed in Knots. */
+double Speed::getKnots() const
+{
+    return (_speed*toKnot);
+}
+
+
+/** Get speed in Nautical Miles per hour */
+double Speed::getMph() const
+{
+    return (_speed*toMph);
+}
+
+
+/** Get speed in feet per minute */
+double Speed::getFpm() const
+{
+    return (_speed*toFpm);
+}
+
+
+/** + operator for speed. */
+Speed Speed::operator + (const Speed& x) const
+{
+    return Speed(x._speed + _speed);
+}
+
+
+/** - operator for speed. */
+Speed Speed::operator - (const Speed& x) const
+{
+    return Speed(_speed - x._speed);
+}
+
+
+/** - operator for speed. */
+Speed operator - (double left, const Speed& right)
+{
+    return Speed(left - right.getMps());
+}
+
+
+/** + operator for speed. */
+Speed operator + (double left, const Speed& right)
+{
+    return Speed(left + right.getMps());
+}
+
+
+/** * operator for speed. */
+Speed operator * (double left, const Speed& right)
+{
+    return Speed(left * right.getMps());
+}
+
+
+/** / operator for speed. */
+double Speed::operator / (const Speed& x) const
+{
+    return _speed / x._speed;
+}
+
+
+/** * operator for speed. */
+double Speed::operator * (const Speed& x) const
+{
+    return _speed * x._speed;
+}
+
+
+/** == operator for Speed */
+bool Speed::operator == (const Speed& x) const
+{
+    return (x._speed==_speed);
+}
+
+
+/** != operator for Speed */
+bool Speed::operator != (const Speed& x) const
+{
+    return (x._speed !=_speed);
+}
+
+
+/** - prefix operator for speed */
+Speed Speed::operator - () const
+{
+    return Speed (-_speed);
+}
+
+
+/** Returns a formatted string for the default horizontal unit-setting. */
+QString Speed::getHorizontalText(bool withUnit, uint precision) const
+{
+    QString result, unit;
+    double speed;
+    unit = getUnitText(_horizontalUnit);
+    speed = getHorizontalValue();
+
+    QByteArray prec;
+    prec.setNum(precision);
+    if (withUnit) {
+      result.sprintf("%1." + prec + "f %s",speed,unit.toLatin1().data());
+    } else {
+        result.sprintf("%1." + prec + "f",speed);
+    }
+    return result;
+
+}
+
+
+/** Returns a formatted string for the default vertical speed units. */
+QString Speed::getVerticalText(bool withUnit, uint precision) const
+{
+    QString result, unit;
+    double speed;
+
+    unit = getUnitText(_verticalUnit);
+    speed = getVerticalValue();
+
+    QByteArray prec;
+    prec.setNum(precision);
+    double round=0.5/(10^precision);
+
+    if ( fabs(speed) < round )
+        speed=0.0;
+
+    if (withUnit) {
+      result.sprintf("%1." + prec + "f %s",speed,unit.toLatin1().data());
+    } else {
+        result.sprintf("%1." + prec + "f",speed);
+    }
+    return result;
+}
+
+
+/** Set speed in selected horizontal unit. */
+void Speed::setHorizontalValue(double speed)
+{
+    setValueInUnit(speed, _horizontalUnit);
+}
+
+
+/** Set speed in selected vertical unit. */
+void Speed::setVerticalValue(double speed)
+{
+    setValueInUnit(speed, _verticalUnit);
+}
+
+
+void Speed::setValueInUnit(double speed, speedUnit unit)
+{
+    switch (unit) {
+    case knots:
+        setKnot(speed);
+        break;
+    case milesPerHour:
+        setMph(speed);
+        break;
+    case metersPerSecond:
+        setMps(speed);
+        break;
+    case kilometersPerHour:
+        setKph(speed);
+        break;
+    case feetPerMinute:
+        setFpm(speed);
+        break;
+    default:
+        setMps(speed);
+        break;
+    }
+}
+
+
+QString Speed::getUnitText(speedUnit unit)
+{
+    switch (unit) {
+    case knots:
+        return "Knt";
+        break;
+    case milesPerHour:
+        return "Mph";
+        break;
+    case metersPerSecond:
+        return "m/s";
+        break;
+    case kilometersPerHour:
+        return "Km/h";
+        break;
+    case feetPerMinute:
+        return "Fpm";
+        break;
+    default:
+        return "m/s";
+    }
+}
+
+QString Speed::getHorizontalUnitText()
+{
+  return getUnitText( _horizontalUnit );
+}
+
+QString Speed::getVerticalUnitText()
+{
+  return getUnitText( _verticalUnit );
+}
+
+double Speed::getValueInUnit(speedUnit unit) const
+{
+    switch (unit) {
+    case knots:
+        return getKnots();
+        break;
+    case milesPerHour:
+        return getMph();
+        break;
+    case metersPerSecond:
+        return getMps();
+        break;
+    case kilometersPerHour:
+        return getKph();
+        break;
+    case feetPerMinute:
+        return getFpm();
+        break;
+    default:
+        return getMps();
+    }
+}
+
+
+void Speed::changed()
+{
+    // @AP Note: we have negative speed values in the variometer
+    // culculation. Setting to zero is not a good idea.
+
+    // speed should not be less than 0
+    // if (_speed<0) _speed=0;
+}
+
---
+++ cumulus/speed.h
@@ -0,0 +1,321 @@
+/***************************************************************************
+                          speed.h  -  description
+                             -------------------
+    begin                : Sat Jul 20 2002
+    copyright            : (C) 2002 by Andre Somers, 2007 Axel Pauli
+    email                : andre@kflog.org, axel@kflog.org
+
+    This file is part of Cumulus
+
+    $Id$
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef SPEED_H
+#define SPEED_H
+
+#include <QString>
+
+//the following constants define the factor by which to multiply meters per
+//second to get the indicated unit
+const double toKph=3.6;
+const double toMph=2.2369;
+const double toKnot=1.9438;
+const double toFpm=196.8504;
+
+/**
+ * @short Abstract speed
+ *
+ * This class can contain a speed. It provides set and get functions for
+ * all popular units. For this class, there are two default units you can
+ * set: the horizontal and the vertical unit. The horizontal unit is used
+ * for the airspeed, the vertical unit is used for climb, McCready settings,
+ * etc. There are convineance functions to access the value using both
+ * appropriate units.
+ *
+ * @author Andre Somers
+ */
+class Speed
+{
+
+public:
+    /**
+     * The speedUnit enum lists the units that apply to speed.
+     */
+    enum speedUnit{
+        metersPerSecond=0,   /**< meters per second */
+        kilometersPerHour=1, /**< kilometers per hour */
+        knots=2,             /**< knots (nautical miles per hour) */
+        milesPerHour=3,      /**< imperial miles per hour */
+        feetPerMinute=4        /**< feet per minute */
+    };
+
+    /**
+     * Constructor
+     */
+    Speed();
+
+    /**
+     * Constructor
+     *
+     * Sets the speed to Mps meters per second.
+     */
+    Speed(double Mps);
+
+    /**
+     * Destructor
+     */
+    virtual ~Speed();
+
+    /**
+     * Get speed in Knots.
+     */
+    double getKnots() const;
+
+    /**
+     * Get speed in Kilometers per hour
+     */
+    double getKph() const;
+
+    /**
+     * Get speed in Meters per Second
+     */
+    double getMps() const;
+
+    /**
+     * Get speed in Nautical Miles per hour
+     */
+    double getMph() const;
+
+    /**
+     * Get speed in Nautical Miles per hour
+     */
+    double getFpm() const;
+
+    /**
+     * Set speed in Nautical miles per hour
+     */
+    void setMph(double speed);
+
+    /**
+     * Set speed in knots
+     */
+    void setKnot(double speed);
+
+    /**
+     * Set speed in Kilometers per hour
+     */
+    void setKph(double speed);
+
+    /**
+     * Set speed in meters per second.
+     */
+    void setMps(double speed);
+
+    /**
+     * Set speed in feet per minute.
+     */
+    void setFpm(double speed);
+
+    /**
+     * Set speed in selected horizontal unit.
+     */
+    void setHorizontalValue(double speed);
+
+    /**
+     * Set speed in selected vertical unit.
+     */
+    void setVerticalValue(double speed);
+
+    /**
+     * set the horizontal unit
+     */
+    static void setHorizontalUnit(speedUnit unit);
+
+    /**
+     * set the vertical unit
+     */
+    static void setVerticalUnit(speedUnit unit);
+
+    /**
+     * get the horizontal unit
+     */
+    static speedUnit getHorizontalUnit();
+
+    /**
+     * get the vertical unit
+     */
+    static speedUnit getVerticalUnit();
+
+    /**
+     * Returns a formatted string for the default vertical speed units.
+     * The string includes the value and optionally the unit.
+     * @param withUnit set to true (default) to have the returned string
+     *    include the unit, false otherwise
+     * @param prec set to the number of digits after the decimal point you
+     *    want in the string
+     */
+    QString getVerticalText(bool withUnit=true, uint prec=1)const;
+
+    /**
+     * Returns a formatted string for the default horizontal speed units.
+     * The string includes the value and optionally the unit.
+     * @param withUnit set to true (default) to have the returned string
+     *    include the unit, false otherwise
+     * @param prec set to the number of digits after the decimal point you
+     *    want in the string
+     */
+    QString getHorizontalText(bool withUnit=true, uint prec=1)const;
+
+    /**
+     * @returns a string for the unit requested. This string only represents
+     *    the unit, not the value.
+     * @param unit the type of unit you want the string for.
+     */
+    static QString getUnitText(speedUnit unit);
+
+    /**
+     * @returns a string for the horizontal unit requested. This string only represents
+     *    the unit, not the value.
+     * @param unit the type of unit you want the string for.
+     */
+    static QString getHorizontalUnitText();
+    static QString getVerticalUnitText();
+
+    /**
+     * Get the value for the vertical speed in the currently active unit.
+     * @returns the current speed value
+     */
+    inline double getVerticalValue() const
+    {
+        return getValueInUnit(_verticalUnit);
+    };
+
+    /**
+     * Get the value for the horizontal speed in the currently active unit.
+     * @returns the current speed value
+     */
+    inline double getHorizontalValue() const
+    {
+        return getValueInUnit(_horizontalUnit);
+    };
+
+    /**
+     * Get the value in the requested unit.
+     * @returns the value converted to the requested unit
+     * @param unit the unit in which to return the value
+     */
+    double getValueInUnit(speedUnit unit) const;
+
+    /**
+     * Set the value in the indicated unit.
+     * @param speed the new speed to set
+     * @param unit the unit in which to set the value
+     */
+    void setValueInUnit(double speed, speedUnit unit);
+
+    /**
+     * Sets the distance to be invalid
+     */
+    inline void setInvalid()
+    {
+        _isValid=false;
+        _speed=0;
+    };
+
+    /**
+     * Gets if the distance is valid
+     */
+    inline bool isValid()
+    {
+        return _isValid;
+    };
+
+    /*Operators */
+    /**
+     * + operator for speed.
+     */
+    Speed operator + (const Speed& x) const;
+
+    /**
+     * - operator for speed.
+     */
+    Speed operator - (const Speed& x) const;
+
+    /**
+     * / operator for speed.
+     */
+    double operator / (const Speed& x) const;
+
+    /**
+     * * operator for speed.
+     */
+    double operator * (const Speed& x) const;
+
+    /**
+     * != operator for Speed
+     */
+    bool operator != (const Speed& x) const;
+
+    /**
+     * == operator for Speed
+     */
+    bool operator == (const Speed& x) const;
+
+    /**
+     * minus prefix operator for speed
+     */
+    Speed operator - () const;
+
+protected:
+    /**
+     * This attribute contains the stored speed in meters per second.
+     */
+    double _speed;
+
+    /**
+     * value valid?
+     */
+    bool _isValid;
+
+    /**
+     * Contains the unit used for horizontal speeds
+     */
+    static speedUnit _horizontalUnit;
+
+    /**
+     * Contains the unit used for vertical speeds
+     */
+    static speedUnit _verticalUnit;
+
+    /**
+     * called if the value has been changed. Can be overridden in derived classes
+     */
+    virtual void changed();
+};
+
+/**
+ * - operator for speed.
+ */
+Speed operator - (double left, const Speed& right);
+
+/**
+ * + operator for speed.
+ */
+Speed operator + (double left, const Speed& right);
+
+/**
+ * * operator for speed.
+ */
+Speed operator * (double left, const Speed& right);
+
+#endif
