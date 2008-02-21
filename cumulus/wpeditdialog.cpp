e2f525a37029c67538a4d8466898fd94f1e67c62
eggert <eggert@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2008-02-21 19:33:25 +0000

    corrected utf8 encoding
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@2475 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/wpeditdialog.cpp
+++ cumulus/wpeditdialog.cpp
@@ -6,7 +6,7 @@
 **
 ************************************************************************
 **
-**   Copyright (c):  2002 by Andr� Somers, 2008 Axel Pauli
+**   Copyright (c):  2002 by André Somers, 2008 Axel Pauli
 **
 **   This file is distributed under the terms of the General Public
 **   Licence. See the file COPYING for more information.
--- cumulus/wpeditdialog.h
+++ cumulus/wpeditdialog.h
@@ -6,7 +6,7 @@
 **
 ************************************************************************
 **
-**   Copyright (c):  2002 by Andr� Somers, 2008 Axel Pauli
+**   Copyright (c):  2002 by André Somers, 2008 Axel Pauli
 **
 **   This file is distributed under the terms of the General Public
 **   Licence. See the file COPYING for more information.
@@ -25,7 +25,7 @@
 
 /**
  * The WPEditDialog allows the editing of waypoint.
- * @author Andr� Somers
+ * @author André Somers
  */
 
 class WPEditDialog : public QDialog
