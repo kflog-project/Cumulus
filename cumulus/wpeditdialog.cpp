9888423c624e1686b6e8173cac7e66fe7368edf9
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2008-09-02 21:20:15 +0000

    ok and cancel button realigned at the right side
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@3003 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/wpeditdialog.cpp
+++ cumulus/wpeditdialog.cpp
@@ -6,7 +6,7 @@
  **
  ************************************************************************
  **
- **   Copyright (c):  2002 by André Somers, 2008 Axel Pauli
+ **   Copyright (c):  2002 by Andrï¿½ Somers, 2008 Axel Pauli
  **
  **   This file is distributed under the terms of the General Public
  **   Licence. See the file COPYING for more information.
@@ -19,13 +19,13 @@
 #include <QTabWidget>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
-#include <QDialogButtonBox>
 
 #include "wpeditdialog.h"
 #include "wpeditdialogpagegeneral.h"
 #include "wpeditdialogpageaero.h"
 #include "mapcontents.h"
 #include "mapmatrix.h"
+#include "generalconfig.h"
 
 extern MapContents *_globalMapContents;
 extern MapMatrix   *_globalMapMatrix;
@@ -42,26 +42,27 @@ WPEditDialog::WPEditDialog(QWidget *parent, wayPoint *wp ):
 #else
   setSizeGripEnabled(true);
 #endif
-  
-  if (wp==0) {
-    setWindowTitle(tr("New Waypoint"));
-  } else {
-    setWindowTitle(tr("Edit Waypoint"));
-  }
+
+  if (wp == 0)
+    {
+      setWindowTitle(tr("New Waypoint"));
+    }
+  else
+    {
+      setWindowTitle(tr("Edit Waypoint"));
+    }
+
   _wp = wp;
 
-  WPEditDialogPageGeneral * pageGeneral=new WPEditDialogPageGeneral(this);
-  WPEditDialogPageAero * pageAero=new WPEditDialogPageAero(this);
-  comment=new QTextEdit(this);
+  WPEditDialogPageGeneral *pageGeneral = new WPEditDialogPageGeneral(this);
+  WPEditDialogPageAero    *pageAero    = new WPEditDialogPageAero(this);
+  comment = new QTextEdit(this);
   comment->setWordWrapMode(QTextOption::WordWrap);
 
-  QTabWidget* tabWidget = new QTabWidget (this);
+  QTabWidget* tabWidget = new QTabWidget(this);
   tabWidget->addTab(pageGeneral, tr("&General"));
   tabWidget->addTab(pageAero, tr("&Aero"));
   tabWidget->addTab(comment, tr("&Comments"));
-
-  QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
-                                                     | QDialogButtonBox::Cancel);
 
   connect(this, SIGNAL(load(wayPoint *)),
           pageGeneral, SLOT(slot_load(wayPoint *)));
@@ -75,16 +76,34 @@ WPEditDialog::WPEditDialog(QWidget *parent, wayPoint *wp ):
   connect(this, SIGNAL(save(wayPoint *)),
           pageAero, SLOT(slot_save(wayPoint *)));
 
-  connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
-  connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+  // Add ok and cancel buttons
+  QPushButton *cancel = new QPushButton;
+  cancel->setIcon(QIcon(GeneralConfig::instance()->loadPixmap("cancel.png")));
+  cancel->setIconSize(QSize(26, 26));
+  cancel->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::QSizePolicy::Preferred);
 
-  QVBoxLayout *mainLayout = new QVBoxLayout(this);
+  QPushButton *ok = new QPushButton;
+  ok->setIcon(QIcon(GeneralConfig::instance()->loadPixmap("ok.png")));
+  ok->setIconSize(QSize(26, 26));
+  ok->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::QSizePolicy::Preferred);
+
+  connect(ok, SIGNAL(clicked()), this, SLOT(accept()));
+  connect(cancel, SIGNAL(clicked()), this, SLOT(reject()));
+
+  QVBoxLayout *buttonBox = new QVBoxLayout;
+  buttonBox->setSpacing(0);
+  buttonBox->addWidget(cancel, 2);
+  buttonBox->addSpacing(20);
+  buttonBox->addWidget(ok, 2);
+  buttonBox->addStretch(2);
+
+  QHBoxLayout *mainLayout = new QHBoxLayout;
   mainLayout->addWidget(tabWidget);
-  mainLayout->addWidget(buttonBox);
-  setLayout (mainLayout);
+  mainLayout->addLayout(buttonBox);
+  setLayout(mainLayout);
 
   tabWidget->setCurrentWidget(pageGeneral);
-  
+
   // load data into subwidgets
   slot_LoadCurrent();
 }
@@ -100,25 +119,31 @@ void WPEditDialog::slot_LoadCurrent()
 {
   emit load(_wp);
 
-  if ( _wp ) {
-    comment->setText(_wp->comment);
-  }
+  if (_wp)
+    {
+      comment->setText(_wp->comment);
+    }
 }
 
 /** Called if OK button is pressed? */
 void WPEditDialog::accept()
 {
-  qDebug ("WPEditDialog::accept");
-  if (_wp==0) {
-    _wp=new wayPoint;
-    emit save(_wp);
-    _wp->projP = _globalMapMatrix->wgsToMap(_wp->origP);
-    //qDebug("new waypoint %s", (const char *)_wp->name);
-  } else {
-    //qDebug("change waypoint %s", (const char *)_wp->name);
-    emit save(_wp);
-    _wp->projP = _globalMapMatrix->wgsToMap(_wp->origP);
-  }
+  // qDebug ("WPEditDialog::accept");
+  if (_wp == 0)
+    {
+      _wp = new wayPoint;
+      emit
+      save(_wp);
+      _wp->projP = _globalMapMatrix->wgsToMap(_wp->origP);
+      //qDebug("new waypoint %s", (const char *)_wp->name);
+    }
+  else
+    {
+      //qDebug("change waypoint %s", (const char *)_wp->name);
+      emit
+      save(_wp);
+      _wp->projP = _globalMapMatrix->wgsToMap(_wp->origP);
+    }
 
   _wp->comment=comment->toPlainText();
   emit wpListChanged(_wp);
--- cumulus/wpeditdialogpagegeneral.cpp
+++ cumulus/wpeditdialogpagegeneral.cpp
@@ -6,7 +6,7 @@
 **
 ************************************************************************
 **
-**   Copyright (c):  2002 by AndrÃ© Somers, 2008 Axel Pauli
+**   Copyright (c):  2002 by André Somers, 2008 Axel Pauli
 **
 **   This file is distributed under the terms of the General Public
 **   Licence. See the file COPYING for more information.
@@ -27,7 +27,7 @@ WPEditDialogPageGeneral::WPEditDialogPageGeneral(QWidget *parent) :
  QWidget(parent)
 {
   setObjectName("WPEditDialogPageGeneral");
-  
+
   QGridLayout * topLayout = new QGridLayout(this);
   topLayout->setMargin(5);
   topLayout->setColumnMinimumWidth( 2, 50 );
