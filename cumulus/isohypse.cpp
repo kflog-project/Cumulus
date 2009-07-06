72021b58eef149fa9c0d1c05b6f598fd2ecc3c0b
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2009-07-06 20:17:01 +0000

    optimisation removed due to artefacts at tile borders
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@3464 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/isohypse.cpp
+++ cumulus/isohypse.cpp
@@ -50,76 +50,21 @@ QRegion* Isohypse::drawRegion( QPainter* targetP, const QRect &viewRect,
 
   QPolygon tP = glMapMatrix->map(projPolygon);
 
-  if(really_draw)
+  if (really_draw)
     {
-      if( tP.boundingRect().isNull() )
+      if (tP.boundingRect().isNull())
         {
           // ignore null values and return also no region
           return static_cast<QRegion *> (0);
         }
 
-      // @AP: try from me to improve isoline drawing. My approach
-      // is to skip small drawing areas on certain scales.
+      targetP->setClipRegion(viewRect);
 
-      int scale = (int)rint(_globalMapMatrix->getScale(MapMatrix::CurrentScale));
-      //qDebug("Scale=%d", scale);
+      targetP->drawPolygon(tP);
 
-      int skipW = 0;
-      int skipH = 0;
-
-      if (scale>1500)
+      if (isolines)
         {
-          skipW = skipH = 20;
-        }
-      else if (scale>1000)
-        {
-          skipW = skipH = 20;
-        }
-      else if (scale>725)
-        {
-          skipW = skipH = 15;
-        }
-      else if (scale>500)
-        {
-          skipW = skipH = 12;
-        }
-      else if (scale>250)
-        {
-          skipW = skipH = 8;
-        }
-      else if (scale>150)
-        {
-          skipW = skipH = 6;
-        }
-      else if (scale>100)
-        {
-          skipW = skipH = 4;
-        }
-
-      if( GeneralConfig::instance()->getMapLoadIsoLines() == false )
-        {
-          // draw all ground data in this case, otherwise on
-          // higher map scales are blue areas to see.
-          skipW = skipH = 0;
-        }
-
-      if( tP.boundingRect().width() > skipW &&
-          tP.boundingRect().height() > skipH )
-        {
-          /* qDebug( "DrawReg: x=%d, y=%d, w=%d, h=%d",
-                  drawReg.boundingRect().x(),
-                  drawReg.boundingRect().y(),
-                  drawReg.boundingRect().width(),
-                  drawReg.boundingRect().height() ); */
-
-          targetP->setClipRegion( viewRect );
-
-          targetP->drawPolygon(tP);
-
-          if( isolines )
-            {
-              targetP->drawPolyline(tP);
-            }
+          targetP->drawPolyline(tP);
         }
     }
 
