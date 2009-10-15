4a457d68acd38d93074c07877879dd3ee316c7bc
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2009-10-15 19:05:18 +0000

    minor format modifications
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@3575 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/glider.cpp
+++ cumulus/glider.cpp
@@ -6,10 +6,11 @@
 **
 ************************************************************************
 **
-**   Copyright (c):  2003 by André Somers, 2008 Axel Pauli
+**   Copyright (c):  2003      by André Somers
+**                   2008-2009 by Axel Pauli
 **
 **   This file is distributed under the terms of the General Public
-**   Licence. See the file COPYING for more information.
+**   License. See the file COPYING for more information.
 **
 **   $Id$
 **
@@ -23,130 +24,146 @@
 
 Glider::Glider()
 {
-    _polar=new Polar("",0,0,0,0,0,0,0,0,0,0);
+  _polar = new Polar( "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
 }
 
 
 Glider::~Glider()
 {
-    delete _polar;
+  delete _polar;
 }
 
 
 void Glider::setType(QString newValue)
 {
-    _type=newValue;
+  _type = newValue;
 }
 
 
-/** This funtion loads the glider-data from the config-object.
+/** This function loads the glider-data from the config-object.
   * The config object needs to be initialized to the correct
   * section beforehand! It tries to load the glider with id id,
   * and returns true if it succeeds, and false otherwise. */
 bool Glider::load(QSettings *config, int id)
 {
-    QString keyname="Glider%1";
+  QString keyname = "Glider%1";
 
-    //check if entry exists
-    if (config->contains(keyname.arg(id))) {
+  //check if entry exists
+  if( config->contains( keyname.arg( id ) ) )
+    {
 
-      QStringList data = config->value(keyname.arg(id), "").toString().split(';');
+      QStringList data = config->value( keyname.arg( id ), "" ).toString().split(';');
 
-        // qDebug("Glider::load(): No of fetched glider items is %d", data.count());
+      // qDebug("Glider::load(): No of fetched glider items is %d", data.count());
 
-        // check to see if we have enough data members
-        if (data.count()<15)
-            return false;
-
-        _type=data[0];
-        _registration=data[1];
-
-        // @AP 18.12.03: A call sign is stored as space, if it is an empty
-        // string. That must be done otherwise the read from config file
-        // will retrieve to less elements and you will never see your
-        // stored glider entry. The space will be removed now.
-
-        if( data[2] == " " ) // Check for space
+      // check to see if we have enough data members
+      if( data.count() < 15 )
         {
-            _callsign= ""; // remove space
-        } else {
-            _callsign=data[2];
+          return false;
         }
 
-        if (data[3].toInt()==1) {
-            _seats=singleSeater;
-        } else {
-            _seats=doubleSeater;
+      _type = data[0];
+      _registration = data[1];
+
+      // @AP 18.12.03: A call sign is stored as space, if it is an empty
+      // string. That must be done otherwise the read from config file
+      // will retrieve to less elements and you will never see your
+      // stored glider entry. The space will be removed now.
+      if( data[2] == " " ) // Check for space
+        {
+          _callSign = ""; // remove space
         }
-        _maxWater=data[4].toInt();
+      else
+        {
+          _callSign = data[2];
+        }
 
-        delete _polar;
+      if( data[3].toInt() == 1 )
+        {
+          _seats = singleSeater;
+        }
+      else
+        {
+          _seats = doubleSeater;
+        }
 
-        Speed V1, V2, V3, W1, W2, W3;
-        V1.setKph(data[5].toDouble());
-        V2.setKph(data[7].toDouble());
-        V3.setKph(data[9].toDouble());
+      _maxWater = data[4].toInt();
 
-        W1.setMps(data[6].toDouble());
-        W2.setMps(data[8].toDouble());
-        W3.setMps(data[10].toDouble());
+      delete _polar;
 
-        _polar=new Polar( _type,
-                         V1, W1,    // v/w pair 1
-                         V2, W2,    // v/w pair 2
-                         V3, W3,    // v/w pair 3
-                         data[11].toDouble(),data[12].toDouble(),  // wingload, wingarea
-                         data[13].toDouble(),data[14].toDouble()); // empty weight, gross weight
-        _lastSafeID=id;
-        return true;
-    } else
-        return false;
+      Speed V1, V2, V3, W1, W2, W3;
+      V1.setKph( data[5].toDouble() );
+      V2.setKph( data[7].toDouble() );
+      V3.setKph( data[9].toDouble() );
+
+      W1.setMps( data[6].toDouble() );
+      W2.setMps( data[8].toDouble() );
+      W3.setMps( data[10].toDouble() );
+
+      _polar = new Polar( _type, V1, W1, // v/w pair 1
+                          V2, W2, // v/w pair 2
+                          V3, W3, // v/w pair 3
+                          data[11].toDouble(), data[12].toDouble(), // wingload, wingarea
+                          data[13].toDouble(), data[14].toDouble() ); // empty weight, gross weight
+      _lastSafeID = id;
+      return true;
+    }
+
+  return false;
 }
 
 
-/** Stores the gliderinfo contained in config-object config under ID id. */
+/** Stores the glider info contained in config-object config under ID id. */
 void Glider::safe(QSettings *config, int id)
 {
-    QStringList data;
-    data.append(_type);
-    data.append(_registration);
+  QStringList data;
 
-    // @AP 18.12.03: If call sign is empty a space is stored as
-    // workaround. Otherwise the load from config data will fail. The
-    // space is removed during load action.
+  data.append( _type );
+  data.append( _registration );
 
-    if( _callsign.isEmpty() ) {
-        data.append(" ");
-    } else {
-        data.append(_callsign);
+  // @AP 18.12.03: If call sign is empty a space is stored as
+  // workaround. Otherwise the load from config data will fail. The
+  // space is removed during load action.
+
+  if( _callSign.isEmpty() )
+    {
+      data.append( " " );
+    }
+  else
+    {
+      data.append( _callSign );
     }
 
-    if (_seats==singleSeater) {
-        data.append("1");
-    } else {
-        data.append("2");
+  if( _seats == singleSeater )
+    {
+      data.append( "1" );
     }
-    data.append(QString::number(_maxWater));
-    data.append(QString::number(_polar->v1().getKph()));
-    data.append(QString::number(_polar->w1().getMps()));
-    data.append(QString::number(_polar->v2().getKph()));
-    data.append(QString::number(_polar->w2().getMps()));
-    data.append(QString::number(_polar->v3().getKph()));
-    data.append(QString::number(_polar->w3().getMps()));
-    data.append(QString::number(0.0)); // wingload not used
-    data.append(QString::number(0.0)); // wingarea not used
-    data.append(QString::number(_polar->emptyWeight())); // empty weight now used
-    data.append(QString::number(_polar->grossWeight())); // gross weight now used
+  else
+    {
+      data.append( "2" );
+    }
 
-    QString keyname="Glider%1";
-    config->setValue(keyname.arg(id), data.join(";"));
-    _lastSafeID=id;
+  data.append( QString::number( _maxWater ) );
+  data.append( QString::number( _polar->v1().getKph() ) );
+  data.append( QString::number( _polar->w1().getMps() ) );
+  data.append( QString::number( _polar->v2().getKph() ) );
+  data.append( QString::number( _polar->w2().getMps() ) );
+  data.append( QString::number( _polar->v3().getKph() ) );
+  data.append( QString::number( _polar->w3().getMps() ) );
+  data.append( QString::number( 0.0 ) ); // wing load not used
+  data.append( QString::number( 0.0 ) ); // wing area not used
+  data.append( QString::number( _polar->emptyWeight() ) ); // empty weight now used
+  data.append( QString::number( _polar->grossWeight() ) ); // gross weight now used
+
+  QString keyname = "Glider%1";
+  config->setValue( keyname.arg( id ), data.join( ";" ) );
+  _lastSafeID = id;
 }
 
 
 void Glider::setPolar(Polar * newPolar)
 {
-    delete _polar;
-    _polar=newPolar;
+  delete _polar;
+  _polar = newPolar;
 }
 
