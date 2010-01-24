7b586447787b8bdcbd49a5de8a4d7b16920c5cea
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2010-01-24 20:03:49 +0000

    new widget dialog to get a user name and a password
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@3702 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/authdialog.h
@@ -0,0 +1,67 @@
+/***********************************************************************
+**
+**   authdialog.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2010 Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/*
+ * This widget ask for a user name and a password. It is used to get these
+ * data on request.
+ *
+ */
+
+#ifndef AUTH_DIALOG_H
+#define AUTH_DIALOG_H
+
+#include <QDialog>
+#include <QLineEdit>
+#include <QString>
+
+class AuthDialog : public QDialog
+{
+  Q_OBJECT
+
+  private:
+
+  Q_DISABLE_COPY ( AuthDialog )
+
+ public:
+
+  AuthDialog( QString &user, QString &password, QString title="", QWidget *parent = 0 );
+
+  virtual ~AuthDialog();
+
+ protected:
+
+  /** standard slots */
+  virtual void accept();
+  virtual void reject();
+
+ private:
+
+  /** Return instance for the user name */
+  QString &_user;
+
+  /** Return instance for the user password */
+  QString &_password;
+
+  /** Editor for user input */
+  QLineEdit *userEdit;
+
+  /** Editor for password input */
+  QLineEdit *passwordEdit;
+
+};
+
+#endif /* AUTH_DIALOG_H */
