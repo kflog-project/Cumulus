1b83f4e5e0bb37e7d212ee26976c5acbb967a177
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2010-06-21 21:40:54 +0000

    enhancement of altimeter dialog with new settings
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@4123 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/altimetermodedialog.cpp
+++ cumulus/altimetermodedialog.cpp
@@ -26,13 +26,14 @@
 
 extern MapConfig *_globalMapConfig;
 
-AltimeterModeDialog::AltimeterModeDialog (QWidget *parent)
-  : QDialog(parent, Qt::WindowStaysOnTopHint),
-    _mode(0)
+AltimeterModeDialog::AltimeterModeDialog (QWidget *parent) :
+  QDialog(parent, Qt::WindowStaysOnTopHint),
+  _mode(0),
+  _unit(0)
 {
   setObjectName("AltimeterModeDialog");
   setModal(true);
-  setWindowTitle(tr("Set Altimeter"));
+  setWindowTitle(tr("Altimeter Settings"));
   setAttribute(Qt::WA_DeleteOnClose);
 
 #ifndef MAEMO
@@ -58,31 +59,93 @@ AltimeterModeDialog::AltimeterModeDialog (QWidget *parent)
   _std = new QRadioButton( tr( "STD" ), altMode );
   _agl = new QRadioButton( tr( "AGL" ), altMode );
   _ahl = new QRadioButton( tr( "AHL" ), altMode );
-  _msl->setChecked( true );
 
+  _msl->setChecked( true );
   _msl->setEnabled( true );
   _std->setEnabled( true );
   _agl->setEnabled( true );
   _ahl->setEnabled( true );
 
-  QVBoxLayout* mainLayout = new QVBoxLayout(this);
-  mainLayout->setObjectName("mainlayout");
+  QHBoxLayout* mainLayout = new QHBoxLayout(this);
 
-  QHBoxLayout* modeLayout = new QHBoxLayout();
-  modeLayout->setObjectName("modelayout");
-  modeLayout->addWidget(altMode);
+  QVBoxLayout* controlLayout = new QVBoxLayout;
+  controlLayout->addWidget(altMode);
 
   QHBoxLayout* radioLayout = new QHBoxLayout(altMode);
-  radioLayout->setObjectName("radiolayout");
   radioLayout->addWidget(_msl);
   radioLayout->addWidget(_std);
   radioLayout->addWidget(_agl);
   radioLayout->addWidget(_ahl);
 
+  //---------------------------------------------------------------------------
+
+  QGroupBox* altUnit = new QGroupBox( this );
+  _meter = new QRadioButton( tr( "Meter" ), altUnit );
+  _feet  = new QRadioButton( tr( "Feet" ), altUnit );
+  _fl    = new QRadioButton( tr( "FL" ), altUnit );
+
+  _meter->setChecked( true );
+  _meter->setEnabled( true );
+  _feet->setEnabled( true );
+  _fl->setEnabled( true );
+  _fl->setVisible( false );
+
+  QHBoxLayout* radioLayout1 = new QHBoxLayout(altUnit);
+  radioLayout1->addWidget(_meter);
+  radioLayout1->addWidget(_feet);
+  radioLayout1->addWidget(_fl);
+
+  controlLayout->addWidget(altUnit);
+
+  spinUserCorrection = new QSpinBox(this);
+  spinUserCorrection->setMinimum(-1000);
+  spinUserCorrection->setMaximum(1000);
+  spinUserCorrection->setButtonSymbols(QSpinBox::NoButtons);
+
+  pplus  = new QPushButton("++", this);
+  plus   = new QPushButton("+", this);
+  mminus = new QPushButton("--", this);
+  minus  = new QPushButton("-", this);
+
+  int size = 40;
+
+#ifdef MAEMO
+  size = 80;
+#endif
+
+  pplus->setMinimumSize(size, size);
+  plus->setMinimumSize(size, size);
+  minus->setMinimumSize(size, size);
+  mminus->setMinimumSize(size, size);
+
+  pplus->setMaximumSize(size, size);
+  plus->setMaximumSize(size, size);
+  minus->setMaximumSize(size, size);
+  mminus->setMaximumSize(size, size);
+
+  pplus->setFocusPolicy(Qt::NoFocus);
+  plus->setFocusPolicy(Qt::NoFocus);
+  minus->setFocusPolicy(Qt::NoFocus);
+  mminus->setFocusPolicy(Qt::NoFocus);
+
+  QHBoxLayout *pmLayout = new QHBoxLayout;
+  pmLayout->setSpacing(5);
+  pmLayout->addWidget(pplus, Qt::AlignLeft);
+  pmLayout->addWidget(plus, Qt::AlignLeft);
+  pmLayout->addStretch(10);
+  pmLayout->addSpacing(20);
+  pmLayout->addWidget( spinUserCorrection );
+  pmLayout->addSpacing(20);
+  pmLayout->addStretch(10);
+  pmLayout->addWidget(minus, Qt::AlignRight);
+  pmLayout->addWidget(mminus, Qt::AlignRight);
+
+  controlLayout->addLayout( pmLayout );
+
   // Align ok and cancel button at the left and right side of the
   // widget to have enough space between them. That shall avoid wrong
   // button pressing in turbulent air.
-  int size = 40;
+  size = 40;
 
 #ifdef MAEMO
   size = 80;
@@ -102,18 +165,19 @@ AltimeterModeDialog::AltimeterModeDialog (QWidget *parent)
   ok->setMaximumSize(size, size);
   ok->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::QSizePolicy::Preferred);
 
-  QHBoxLayout *butLayout = new QHBoxLayout;
-  butLayout->addWidget( ok );
-  butLayout->addStretch();
-  butLayout->addWidget( cancel );
+  QVBoxLayout *buttonLayout = new QVBoxLayout;
+  buttonLayout->addWidget( cancel );
+  buttonLayout->addStretch();
+  buttonLayout->addWidget( ok );
 
-  mainLayout->addLayout(modeLayout);
+  mainLayout->addLayout(controlLayout);
   mainLayout->addSpacing(25);
-  mainLayout->addLayout(butLayout);
+  mainLayout->addLayout(buttonLayout);
 
   timeout = new QTimer(this);
   timeout->setSingleShot(true);
 
+  // Altitude reference radio buttons
   QSignalMapper* signalMapper = new QSignalMapper(this);
   connect(_msl, SIGNAL(clicked()), signalMapper, SLOT(map()));
   signalMapper->setMapping(_msl, 0);
@@ -124,7 +188,30 @@ AltimeterModeDialog::AltimeterModeDialog (QWidget *parent)
   connect(_ahl, SIGNAL(clicked()), signalMapper, SLOT(map()));
   signalMapper->setMapping(_ahl, 3);
 
-  connect(signalMapper, SIGNAL(mapped(int)), this, SLOT(change_mode(int)));
+  // Altitude unit radio buttons
+  QSignalMapper* signalMapperUnit = new QSignalMapper(this);
+  connect(_meter, SIGNAL(clicked()), signalMapperUnit, SLOT(map()));
+  signalMapperUnit->setMapping(_meter, 0);
+  connect(_feet, SIGNAL(clicked()), signalMapperUnit, SLOT(map()));
+  signalMapperUnit->setMapping(_feet, 1);
+  connect(_fl, SIGNAL(clicked()), signalMapperUnit, SLOT(map()));
+  signalMapperUnit->setMapping(_fl, 2);
+
+  // Altitude correction buttons
+  QSignalMapper* signalMapperButtons = new QSignalMapper(this);
+  connect(pplus, SIGNAL(clicked()), signalMapperButtons, SLOT(map()));
+  signalMapperButtons->setMapping(pplus, 0);
+  connect(plus, SIGNAL(clicked()), signalMapperButtons, SLOT(map()));
+  signalMapperButtons->setMapping(plus, 1);
+  connect(minus, SIGNAL(clicked()), signalMapperButtons, SLOT(map()));
+  signalMapperButtons->setMapping(minus, 2);
+  connect(mminus, SIGNAL(clicked()), signalMapperButtons, SLOT(map()));
+  signalMapperButtons->setMapping(mminus, 3);
+
+  connect(signalMapper, SIGNAL(mapped(int)), this, SLOT(slotChangeMode(int)));
+  connect(signalMapperUnit, SIGNAL(mapped(int)), this, SLOT(slotChangeUnit(int)));
+  connect(signalMapperButtons, SIGNAL(mapped(int)), this, SLOT(slotChangeValue(int)));
+
   connect (timeout, SIGNAL(timeout()), this, SLOT(reject()));
   connect (ok, SIGNAL(clicked()), this, SLOT(accept()));
   connect (cancel, SIGNAL(clicked()), this, SLOT(reject()));
@@ -159,11 +246,10 @@ AltimeterModeDialog::~AltimeterModeDialog()
 
 void AltimeterModeDialog::load()
 {
-  GeneralConfig *conf = GeneralConfig::instance();
+  _mode = GeneralConfig::instance()->getAltimeterMode();
 
-  _mode = conf->getAltimeterMode();
-
-  switch (_mode) {
+  switch( _mode )
+  {
     case 0:
       _msl->setChecked(true);
       break;
@@ -180,59 +266,97 @@ void AltimeterModeDialog::load()
       qWarning("AltimeterModeDialog::load(): invalid mode: %d", _mode);
   }
 
-  setTimer();
-}
-
-void AltimeterModeDialog::work()
-{
-  // qDebug("_toggling_mode %d", _toggling_mode );
-
   GeneralConfig *conf = GeneralConfig::instance();
 
-  _mode = conf->getAltimeterMode();
-  _toggling_mode = conf->getAltimeterToggleMode();
+  switch( Altitude::getUnit() )
+  {
+    case Altitude::meters:
+      _meter->setChecked(true);
+      _unit = 0;
+      spinUserCorrection->setValue( (int) conf->getGpsUserAltitudeCorrection().getMeters() );
+      break;
+    case Altitude::feet:
+      _feet->setChecked(true);
+      _unit = 1;
+      spinUserCorrection->setValue( (int) conf->getGpsUserAltitudeCorrection().getFeet() );
+      break;
+    case Altitude::flightlevel:
+      _fl->setChecked(true);
+      _unit = 2;
+      break;
+    default:
+      _meter->setChecked(true);
+      _unit = 0;
+      break;
+  }
 
-  if( _toggling_mode )
-    {
-      // qDebug("_mode %d", _mode );
-
-      switch( _mode )
-        {
-        case 0:
-          save( 1 );
-          break;
-        case 1:
-          save( 2 );
-          break;
-        case 2:
-          save( 3 );
-          break;
-        case 3:
-        default:
-          save( 0 );
-          break;
-        }
-    }
-  else
-    {
-      load();
-    }
+  slotSetTimer();
 }
 
 void AltimeterModeDialog::save( int mode )
 {
   _mode = mode;
+  Altitude::setUnit( (Altitude::altitudeUnit) _unit );
+
   GeneralConfig *conf = GeneralConfig::instance();
+
   conf->setAltimeterMode(_mode);
+  conf->setGpsUserAltitudeCorrection( Altitude::convertToMeters(spinUserCorrection->value()) );
   conf->save();
-  // qDebug("Save new val %d", _mode );
+
   emit newAltimeterMode();
   emit settingsChanged();
+  emit newAltimeterSetting();
 }
 
-void AltimeterModeDialog::change_mode( int mode )
+void AltimeterModeDialog::slotChangeMode( int mode )
 {
   _mode = mode;
+  slotSetTimer();
+}
+
+void AltimeterModeDialog::slotChangeUnit( int unit )
+{
+  _unit = unit;
+  slotSetTimer();
+}
+
+void AltimeterModeDialog::slotChangeValue( int newValue )
+{
+  int diff;
+
+  if( (Altitude::altitudeUnit) _unit == Altitude::meters )
+    {
+      diff = 1;
+    }
+  else
+    {
+      diff = 3;
+    }
+
+  switch( newValue )
+    {
+    case 0: // ++ was pressed
+      spinUserCorrection->setValue( spinUserCorrection->value() + diff*5 );
+      break;
+    case 1: // + was pressed
+      spinUserCorrection->setValue( spinUserCorrection->value() + diff );
+      break;
+    case 2: // - was pressed
+      spinUserCorrection->setValue( spinUserCorrection->value() - diff );
+      break;
+    case 3: // -- was pressed
+      spinUserCorrection->setValue( spinUserCorrection->value() - diff*5 );
+      break;
+    }
+
+  Altitude::setUnit( (Altitude::altitudeUnit) _unit );
+
+  GeneralConfig *conf = GeneralConfig::instance();
+  conf->setGpsUserAltitudeCorrection( Altitude::convertToMeters(spinUserCorrection->value()) );
+  emit newAltimeterSetting();
+
+  slotSetTimer();
 }
 
 void AltimeterModeDialog::accept()
@@ -241,12 +365,10 @@ void AltimeterModeDialog::accept()
   QDialog::accept();
 }
 
-void AltimeterModeDialog::setTimer()
+void AltimeterModeDialog::slotSetTimer()
 {
   // @AP: let us take the user's defined info display time
-  GeneralConfig *conf = GeneralConfig::instance();
-
-  _time = conf->getInfoDisplayTime();
+  _time = GeneralConfig::instance()->getInfoDisplayTime();
 
   if( _time > 0 )
     {
--- cumulus/altimetermodedialog.h
+++ cumulus/altimetermodedialog.h
@@ -10,7 +10,7 @@
 **                   2008-2010 by Axel Pauli
 **
 **   This file is distributed under the terms of the General Public
-**   Lisense. See the file COPYING for more information.
+**   License. See the file COPYING for more information.
 **
 **   $Id$
 **
@@ -22,7 +22,7 @@
 #include <QDialog>
 #include <QTimer>
 #include <QRadioButton>
-
+#include <QSpinBox>
 /**
   * \author Eckhard Voellm, Axel Pauli
   *
@@ -43,16 +43,16 @@ class AltimeterModeDialog : public QDialog
 public:
 
   AltimeterModeDialog(QWidget *parent);
-  ~AltimeterModeDialog();
-  void work();
+  virtual ~AltimeterModeDialog();
+
   static QString mode2String();
   static int mode();
 
 protected:
 
-  void load ();
-  void accept ();
-  void save (int mode); // 0: MSL,  1: STD,  2: AGL, 3: AHL
+  void load();
+  void accept();
+  void save( int mode ); // 0: MSL,  1: STD,  2: AGL, 3: AHL
 
 private:
 
@@ -60,20 +60,40 @@ private:
   int _time;
   int _mode;  	 // 0: MSL,  1: STD,  2: AGL, 3: AHL
   bool _toggling_mode;   // 1: On
-  QRadioButton * _msl;
-  QRadioButton * _agl;
-  QRadioButton * _std;
-  QRadioButton * _ahl;
+
+  // Altitude references
+  QRadioButton* _msl;
+  QRadioButton* _agl;
+  QRadioButton* _std;
+  QRadioButton* _ahl;
+
+  // Altitude units
+  int _unit;     // 0: Meter,  1: Feet,  2: FL
+  QRadioButton* _meter;
+  QRadioButton* _feet;
+  QRadioButton* _fl;
+
+  // Spin box for altitude correction
+  QSpinBox* spinUserCorrection;
+
+  // Setup buttons
+  QPushButton *plus;
+  QPushButton *pplus;
+  QPushButton *minus;
+  QPushButton *mminus;
 
 private slots:
 
-  void setTimer();
-  void change_mode (int);
+  void slotSetTimer();
+  void slotChangeMode( int mode );
+  void slotChangeUnit( int unit );
+  void slotChangeValue( int unit );
 
 signals:
 
   void settingsChanged();
   void newAltimeterMode();
+  void newAltimeterSetting();
 };
 
 #endif
