385ea0b31b9ae1bca441042e3ffb6a958ade7544
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2010-10-15 17:45:06 +0000

    common GUI layout definitions
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@4335 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/layout.h
@@ -0,0 +1,36 @@
+/***********************************************************************
+**
+**   layout.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2010 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/**
+ * \author Axel Pauli
+ *
+ * \brief GUI layout definitions
+ *
+ * This class defines common GUI parameters.
+ *
+ */
+
+#ifndef LAYOUT_H_
+#define LAYOUT_H_
+
+#ifndef MAEMO
+#define IconSize 26
+#else
+#define IconSize 32
+#endif
+
+#endif /* LAYOUT_H_ */
