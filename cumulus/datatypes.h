61c1774810c7ba5395b373365e30e16d8c3862b8
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2010-11-03 19:05:41 +0000

    special data type definitions for thread connections
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@4374 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/datatypes.h
@@ -0,0 +1,48 @@
+/***********************************************************************
+**
+**   datatypes.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c): 2010 by Axel Pauli (axel@kflog.org)
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/**
+ * \author Axel Pauli
+ *
+ * \brief A class defining QT user data types.
+ *
+ *  A class defining QT user data types usable in queued connections of
+ *  threads.
+ */
+
+#ifndef DATA_TYPES_H_
+#define DATA_TYPES_H_
+
+#ifdef BLUEZ
+
+#include <QMetaType>
+#include <QString>
+#include <QMap>
+
+/**
+ * Special data type to return the found Bluetooth devices to the GUI thread
+ * via a signal from another running thread.
+ * The map key is the logical bluetooth device name, the map value is the
+ * bluetooth address belonging to key.
+ */
+typedef QMap<QString, QString> BtDeviceMap;
+
+Q_DECLARE_METATYPE(BtDeviceMap)
+
+#endif
+
+#endif // DATA_TYPES_H_
