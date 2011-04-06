54386d3740f9e29295456a02542f286b2aaafd41
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2011-04-06 19:16:46 +0000

    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@4594 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/mainwindow.cpp
+++ cumulus/mainwindow.cpp
@@ -248,7 +248,7 @@ MainWindow::MainWindow( Qt::WindowFlags flags ) : QMainWindow( 0, flags )
   setWindowIcon( QIcon(GeneralConfig::instance()->loadPixmap("cumulus-desktop26x26.png")) );
   setWindowTitle( "Cumulus" );
 
-#ifdef MAEMO4
+#ifdef MAEMO
   setWindowState(Qt::WindowFullScreen);
 #endif
 
