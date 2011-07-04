b8d0bd715d477ab9dd0f6dd56c51e8c7c08e291f
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2011-07-04 17:15:52 +0000

    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@4664 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/coordedit.cpp
+++ cumulus/coordedit.cpp
@@ -2,7 +2,7 @@
 **
 **   coordedit.cpp - Editor for WGS84 coordinates, supports three formats.
 **
-**   This file is part of KFLog4
+**   This file is part of Cumulus
 **
 ************************************************************************
 **
