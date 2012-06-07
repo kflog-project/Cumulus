436be4d58f15b810431fc3cdf8516c0374b2f956
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2012-06-07 16:21:22 +0000

    new font dialog widget for Android
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@5187 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/fontdialog.cpp
@@ -0,0 +1,232 @@
+/***********************************************************************
+**
+**   fontdialog.cpp
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c): 2012 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#include <QtGui>
+
+#include "fontdialog.h"
+
+FontDialog::FontDialog (QWidget *parent) :
+  QDialog(parent, Qt::WindowStaysOnTopHint)
+{
+  qDebug() << "FontDialog::FontDialog";
+
+  setObjectName("FontDialog");
+  // setAttribute(Qt::WA_DeleteOnClose);
+  setModal(true);
+  setWindowTitle(tr("Select Font"));
+
+  fontLabel = new QLineEdit;
+  fontLabel->setReadOnly(true);
+
+  styleLabel = new QLineEdit;
+  styleLabel->setReadOnly(true);
+
+  sizeLabel = new QLineEdit;
+  sizeLabel->setReadOnly(true);
+
+  sampleText = new QLineEdit;
+  sampleText->setReadOnly(true);
+  sampleText->setMinimumHeight(100);
+  //sampleText->setSizePolicy(QSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored));
+  sampleText->setAlignment(Qt::AlignCenter);
+  sampleText->setText(QLatin1String("AaBbYyZz"));
+
+  fontList  = new QListWidget;
+  styleList = new QListWidget;
+  sizeList  = new QListWidget;
+
+  connect( fontList, SIGNAL(itemClicked(QListWidgetItem *)),
+            this, SLOT(slotFontListClicked(QListWidgetItem *)));
+  connect( styleList, SIGNAL(itemClicked(QListWidgetItem *)),
+            this, SLOT(slotStyleListClicked(QListWidgetItem *)));
+  connect( sizeList, SIGNAL(itemClicked(QListWidgetItem *)),
+            this, SLOT(slotSizeListClicked(QListWidgetItem *)));
+
+  QGridLayout* gl = new QGridLayout(this);
+  gl->setMargin(10);
+  gl->setSpacing(15);
+
+  gl->addWidget( new QLabel(tr("Font")), 0, 0 );
+  gl->addWidget( new QLabel(tr("Style")), 0, 1 );
+  gl->addWidget( new QLabel(tr("Size")), 0, 2 );
+
+  gl->addWidget( fontLabel, 1, 0 );
+  gl->addWidget( styleLabel, 1, 1 );
+  gl->addWidget( sizeLabel, 1, 2 );
+
+  gl->addWidget( fontList, 2, 0 );
+  gl->addWidget( styleList, 2, 1 );
+  gl->addWidget( sizeList, 2, 2 );
+
+  QGroupBox* gBox = new QGroupBox(tr("Sample"));
+  QVBoxLayout *vbox = new QVBoxLayout;
+  vbox->addWidget(sampleText);
+  gBox->setLayout(vbox);
+  gl->addWidget( gBox, 3, 0, 1, 3 );
+
+  QDialogButtonBox *buttonBox = new QDialogButtonBox( QDialogButtonBox::Cancel |
+                                                         QDialogButtonBox::Ok );
+  buttonBox->setCenterButtons( true );
+  gl->addWidget( buttonBox, 4, 0, 1, 3 );
+
+  QPushButton *ok = buttonBox->button( QDialogButtonBox::Ok );
+  ok->setDefault( true );
+
+  QPushButton *cancel = buttonBox->button( QDialogButtonBox::Cancel );
+  cancel->setAutoDefault(false);
+
+  connect( buttonBox, SIGNAL(accepted()), this, SLOT(accept()) );
+  connect( buttonBox, SIGNAL(rejected()), this, SLOT(reject()) );
+
+  QFontDatabase fd;
+  QStringList fdl = fd.families( QFontDatabase::Latin );
+
+  fontList->addItems( fdl );
+}
+
+/**
+ * This slot is called, if the user has selected a font entry in the list.
+ */
+void FontDialog::slotFontListClicked( QListWidgetItem* item )
+{
+  if( item == 0 )
+    {
+      return;
+    }
+
+  fontLabel->setText(item->text());
+  styleLabel->clear();
+  sizeLabel->clear();
+  styleList->clear();
+  sizeList->clear();
+
+  // Update now the style and size lists.
+  QFontDatabase fd;
+  QStringList fstl = fd.styles ( item->text() );
+
+  if( fstl.isEmpty() )
+    {
+      return;
+    }
+
+  styleList->addItems(fstl);
+  styleLabel->setText( fstl.at(0) );
+
+  QList<int> fsl = fd.pointSizes ( item->text(), fstl.at(0) );
+
+  if( fsl.isEmpty() )
+    {
+      return;
+    }
+
+  qDebug() << "Setting size list";
+
+  for( int i = 0; i < fsl.size(); i++ )
+    {
+      sizeList->addItem( QString::number( fsl.at(i)));
+    }
+
+  sizeLabel->setText( QString::number( fsl.at(0)));
+  slotUpdateSampleText();
+}
+
+/**
+ * This slot is called, if the user has selected a style entry in the list.
+ */
+void FontDialog::slotStyleListClicked( QListWidgetItem* item )
+{
+  if( item == 0 )
+    {
+      return;
+    }
+
+  styleLabel->setText(item->text());
+  sizeLabel->clear();
+  sizeList->clear();
+
+  QFontDatabase fd;
+  QList<int> fsl = fd.pointSizes ( fontLabel->text(), item->text() );
+
+  if( fsl.isEmpty() )
+    {
+      return;
+    }
+
+  qDebug() << "Setting size list";
+
+  for( int i = 0; i < fsl.size(); i++ )
+    {
+      sizeList->addItem( QString::number( fsl.at(i)));
+    }
+
+  sizeLabel->setText( QString::number( fsl.at(0)));
+  slotUpdateSampleText();
+}
+
+/**
+ * This slot is called, if the user has selected a size entry in the list.
+ */
+void FontDialog::slotSizeListClicked( QListWidgetItem* item )
+{
+  if( item == 0 )
+    {
+      return;
+    }
+
+  sizeLabel->setText( item->text() );
+  slotUpdateSampleText();
+}
+
+
+void FontDialog::slotUpdateSampleText()
+{
+  if( fontLabel->text().isEmpty() || styleLabel->text().isEmpty() || sizeLabel->text().isEmpty() )
+    {
+      sampleText->clear();
+      return;
+    }
+
+  // compute new font
+  int pSize = sizeLabel->text().toInt();
+
+  QFontDatabase fdb;
+  QFont newFont(fdb.font(fontLabel->text(), styleLabel->text(), pSize));
+
+  if( newFont != sampleText->font() )
+    {
+      sampleText->setFont(newFont);
+    }
+
+  sampleText->setText(QLatin1String("AaBbYyZz"));
+}
+
+/**
+ * This slot is called, if the user has pressed the ok button.
+ */
+void FontDialog::slotAccept()
+{
+
+}
+
+/**
+ * This slot is called, if the user has pressed the cancel button.
+ */
+void FontDialog::slotReject()
+{
+
+}
+
---
+++ cumulus/fontdialog.h
@@ -0,0 +1,99 @@
+/***********************************************************************
+**
+**   fontdialog.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c): 2012 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/**
+ * \class FontDialog
+ *
+ * \author Axel Pauli
+ *
+ * \brief This dialog is a user interface for the font selection.
+ *
+ * This dialog is a user interface for the font selection.
+ *
+ * \date 2012
+ *
+ * \version $Id$
+ */
+
+#ifndef FONT_DIALOG_H_
+#define FONT_DIALOG_H_
+
+#include <QDialog>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QPushButton>
+
+class FontDialog : public QDialog
+{
+  Q_OBJECT
+
+ private:
+
+  Q_DISABLE_COPY ( FontDialog )
+
+ public:
+
+  FontDialog(QWidget *parent);
+
+  virtual ~FontDialog() {};
+
+ private slots:
+  /**
+   * This slot is called, if the user has pressed the ok button.
+   */
+  void slotAccept();
+
+  /**
+   * This slot is called, if the user has pressed the cancel button.
+   */
+  void slotReject();
+
+  /**
+   * This slot is called, if the user has selected a font entry in the list.
+   */
+  void slotFontListClicked( QListWidgetItem* item );
+
+  /**
+   * This slot is called, if the user has selected a style entry in the list.
+   */
+  void slotStyleListClicked( QListWidgetItem* item );
+
+  /**
+   * This slot is called, if the user has selected a size entry in the list.
+   */
+  void slotSizeListClicked( QListWidgetItem* item );
+
+  /**
+   * This slot is called if the font has been changed.
+   */
+  void slotUpdateSampleText();
+
+ private:
+
+   QLineEdit* fontLabel;
+   QLineEdit* styleLabel;
+   QLineEdit* sizeLabel;
+   QLineEdit* sampleText;
+
+   QListWidget* fontList;
+   QListWidget* styleList;
+   QListWidget* sizeList;
+
+};
+
+
+#endif /* FONT_DIALOG_H_ */
