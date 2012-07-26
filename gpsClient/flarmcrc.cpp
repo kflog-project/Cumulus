acc1bbadfae3f0c32d42d486cf412ed86183fd60
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2012-07-26 13:17:44 +0000

    Flarm additions
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@5380 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ gpsClient/flarmcrc.cpp
@@ -0,0 +1,35 @@
+/***********************************************************************
+**
+**   flarmcrc.cpp
+**
+**   This file is part of Cumulus
+**
+************************************************************************
+**
+**   Copyright (c):  2012 by Axel Pauli (axel@kflog.org)
+**
+**   This program is free software; you can redistribute it and/or modify
+**   it under the terms of the GNU General Public License as published by
+**   the Free Software Foundation; either version 2 of the License, or
+**   (at your option) any later version.
+**
+**   $Id$
+**
+**   Thanks to Flarm Technology GmbH, who supported us.
+**
+***********************************************************************/
+
+#include "flarmcrc.h"
+
+void FlarmCrc::update( const unsigned char b )
+{
+  m_crc = m_crc ^ (((unsigned short) b) << 8);
+
+  for (int i = 0; i < 8; i++)
+    {
+      if (m_crc & 0x8000)
+        m_crc = (m_crc << 1) ^ 0x1021;
+      else
+        m_crc = (m_crc << 1) & 0xffff;
+    }
+}
---
+++ gpsClient/flarmcrc.h
@@ -0,0 +1,66 @@
+/***********************************************************************
+**
+**   flarmcrc.h
+**
+**   This file is part of Cumulus
+**
+************************************************************************
+**
+**   Copyright (c):  2012 by Axel Pauli (axel@kflog.org)
+**
+**   This program is free software; you can redistribute it and/or modify
+**   it under the terms of the GNU General Public License as published by
+**   the Free Software Foundation; either version 2 of the License, or
+**   (at your option) any later version.
+**
+**   $Id$
+**
+**   Thanks to Flarm Technology GmbH, who supported us.
+**
+***********************************************************************/
+
+#ifndef FLARM_CRC_H_
+#define FLARM_CRC_H_
+
+/**
+ * \class flarmCrc
+ *
+ * \author Flarm, Axel Pauli
+ *
+ * \date 2012
+ *
+ * \brief Flarm CRC calculation formula.
+ *
+ */
+
+class FlarmCrc
+{
+ public:
+
+  FlarmCrc() : m_crc(0)
+  {};
+
+  ~FlarmCrc()
+  {};
+
+  /**
+   * Add character to the CRC checksum.
+   *
+   * \param char Character to be added to the CRC checksum.
+   */
+  void update(const unsigned char b);
+
+  /**
+   * \return The calculated CRC.
+   */
+  unsigned short getCRC() const
+  {
+    return m_crc;
+  };
+
+ private:
+
+  unsigned short m_crc;
+};
+
+#endif /* FLARM_CRC_H_ */
