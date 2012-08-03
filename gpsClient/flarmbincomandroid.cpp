71da24833b17d384b78cd21209b2b074490b6288
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2012-08-03 13:11:56 +0000

    low level routines form FlarmBinCom under Android
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@5451 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ gpsClient/flarmbincomandroid.cpp
@@ -0,0 +1,53 @@
+/***********************************************************************
+**
+**   flarmbincomandroid.cpp
+**
+**   This file is part of Cumulus
+**
+************************************************************************
+**
+**   Copyright (c):  2012 by Axel Pauli (axel@kflog.org)
+**
+**   This program is free software; you can redistribute it and/or modify
+**   it under the terms of the GNU General Public License as published by
+**   the Free Software Foundation; either version 2 of the License, or
+**   (at your option) any later version.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#include <QtGui>
+
+#include "flarmbincomandroid.h"
+#include "gpsconandroid.h"
+#include "jnisupport.h"
+
+FlarmBinComAndroid::FlarmBinComAndroid() :
+  FlarmBinCom()
+{
+}
+
+FlarmBinComAndroid::~FlarmBinComAndroid()
+{
+}
+
+int FlarmBinComAndroid::writeChar(const unsigned char c)
+{
+  if( jniByte2Gps( c ) )
+    {
+      return 1;
+    }
+
+  return -1;
+}
+
+int FlarmBinComAndroid::readChar(unsigned char* b)
+{
+  if( GpsConAndroid::getByte( b ) )
+    {
+       return 1;
+    }
+
+  return -1;
+}
---
+++ gpsClient/flarmbincomandroid.h
@@ -0,0 +1,57 @@
+/***********************************************************************
+**
+**   flarmbincomandroid.h
+**
+**   This file is part of Cumulus
+**
+************************************************************************
+**
+**   Copyright (c):  2012 by Axel Pauli (axel@kflog.org)
+**
+**   This program is free software; you can redistribute it and/or modify
+**   it under the terms of the GNU General Public License as published by
+**   the Free Software Foundation; either version 2 of the License, or
+**   (at your option) any later version.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#ifndef FLARM_BIN_COM_ANDROID_H_
+#define FLARM_BIN_COM_ANDROID_H_
+
+#include "flarmbincom.h"
+
+/**
+ * \class FlarmBinComAndroid
+ *
+ * \author Axel Pauli
+ *
+ * \date 2012
+ *
+ * \brief Flarm binary low level port routines for Android.
+ *
+ * \version $Id$
+ *
+ */
+
+class FlarmBinComAndroid : public FlarmBinCom
+{
+
+ public:
+
+  FlarmBinComAndroid();
+
+  virtual ~FlarmBinComAndroid();
+
+ protected:
+
+  /** Low level write character port method. */
+  virtual int writeChar(const unsigned char c);
+
+  /** Low level read character port method. */
+  virtual int readChar(unsigned char* b);
+
+};
+
+#endif /* FLARM_BIN_COM_LINUX_H_ */
