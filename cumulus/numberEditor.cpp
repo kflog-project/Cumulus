c413be10810e7dc582122a9015779e77bffc3d1b
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2012-12-14 13:12:31 +0000

    new number editor widget optimized for fingers operation
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@5623 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/numberEditor.cpp
@@ -0,0 +1,79 @@
+/***********************************************************************
+**
+**   numberEditor.cpp
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c): 2012 Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#include <QtGui>
+
+#include "numberInputPad.h"
+#include "numberEditor.h"
+
+/**
+ * Constructor
+ */
+NumberEditor::NumberEditor( QWidget *parent,
+                            QString number,
+                            QString prefix,
+                            QString suffix ) :
+  QLabel( parent ),
+  m_nip(0),
+  m_prefix(prefix),
+  m_number(number),
+  m_suffix(suffix),
+  m_decimalFlag(true),
+  m_pmFlag(true),
+  m_validator(0),
+  m_inputMask(""),
+  m_maxLength(32767)
+{
+//  QPalette p = palette();
+//  p.setColor( QPalette::Window, Qt::white );
+//  setPalette(p);
+//  setAutoFillBackground( true );
+
+  setBackgroundRole( QPalette::Light );
+  setAutoFillBackground( true );
+  setAlignment(Qt::AlignCenter);
+  setText();
+}
+
+NumberEditor::~NumberEditor()
+{
+}
+
+void NumberEditor::mousePressEvent( QMouseEvent* event )
+{
+  // Opens the number input pad after amouse press.
+  if( ! m_nip )
+    {
+      m_nip = new NumberInputPad( m_number, this );
+      m_nip->setDecimalVisible( m_decimalFlag );
+      m_nip->setPmVisible( m_pmFlag );
+      m_nip->setValidator( m_validator );
+      m_nip->setMaxLength( m_maxLength );
+      m_nip->setInputMask( m_inputMask );
+      connect( m_nip, SIGNAL(number(const QString&) ), SLOT(slot_Number(const QString&)) );
+      m_nip->show();
+    }
+
+  event->accept();
+}
+
+void NumberEditor::slot_Number( const QString& number )
+{
+  m_nip = 0;
+  m_number = number;
+  setText();
+}
---
+++ cumulus/numberEditor.h
@@ -0,0 +1,143 @@
+/***********************************************************************
+**
+**   numberEditor.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c): 2012 Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/**
+ * \class NumberEditor
+ *
+ * \author Axel Pauli
+ *
+ * \brief Number editor and display.
+ *
+ * This widget can be used to display a number in a QLabel and to edit it
+ * by using an own number keypad.
+ *
+ * \date 2012
+ *
+ * \version $Id$
+ */
+
+#ifndef NUMBER_EDITOR_H_
+#define NUMBER_EDITOR_H_
+
+#include <QLabel>
+#include <QString>
+#include <QValidator>
+
+class QMouseEvent;
+class NumberInputPad;
+
+
+class NumberEditor : public QLabel
+{
+  Q_OBJECT
+
+ private:
+
+  Q_DISABLE_COPY ( NumberEditor )
+
+ public:
+
+  NumberEditor( QWidget *parent=0,
+                const QString number="",
+                const QString prefix="",
+                const QString suffix="" );
+
+  virtual ~NumberEditor();
+
+  void setNumber(const QString number)
+  {
+    m_number = number;
+    setText();
+  };
+
+  QString getNumber()
+  {
+    return m_number;
+  };
+
+  void setPrefix(const QString prefix)
+  {
+    m_prefix = prefix;
+    setText();
+  };
+
+  void setSuffix(const QString suffix)
+  {
+    m_suffix = suffix;
+    setText();
+  };
+
+  void setDecimalVisible( const bool flag )
+  {
+    m_decimalFlag = flag;
+  };
+
+  void setPmVisible( const bool flag )
+  {
+    m_pmFlag = flag;
+  };
+
+  void setValidator( QValidator* validator )
+  {
+    m_validator = validator;
+  };
+
+  void setInputMask( const QString inputMask )
+  {
+    m_inputMask = inputMask;
+  };
+
+  void setMaxLength( int max )
+  {
+    m_maxLength = max;
+  };
+
+ protected:
+
+   /**
+    * Handles mouse button presses.
+    */
+   void mousePressEvent( QMouseEvent* event );
+
+ private slots:
+
+  void slot_Number( const QString& number );
+
+ private:
+
+  void setText()
+  {
+    QLabel::setText( m_prefix + m_number + m_suffix );
+  };
+
+ private:
+
+  NumberInputPad* m_nip;
+
+  QString m_prefix;
+  QString m_number;
+  QString m_suffix;
+
+  bool m_decimalFlag;
+  bool m_pmFlag;
+
+  QValidator* m_validator;
+  QString m_inputMask;
+  int m_maxLength;
+};
+
+#endif /* NUMBER_EDITOR_H_ */
