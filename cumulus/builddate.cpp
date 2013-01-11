4f89b2edf88830a8b36ac73bc6e5598342d9564a
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2013-01-11 21:31:08 +0000

    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@5720 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/builddate.cpp
@@ -0,0 +1,19 @@
+/***********************************************************************
+**
+**   bilddate.cpp
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2013 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+// The build date of the Cumulus application
+const char* CumulusBuildDate = __DATE__;
