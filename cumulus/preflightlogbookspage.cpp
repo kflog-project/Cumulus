fb3e8b86b43ee01e72f737418954af8c832f610e
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2013-10-01 20:13:53 +0000

    new pre-flight widget added for flight logbooks
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@6351 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/preflightlogbookspage.cpp
@@ -0,0 +1,147 @@
+/***********************************************************************
+ **
+ **   preflightlogbookspage.cpp
+ **
+ **   This file is part of Cumulus.
+ **
+ ************************************************************************
+ **
+ **   Copyright (c):  2013 by Axel Pauli
+ **
+ **   This file is distributed under the terms of the General Public
+ **   License. See the file COPYING for more information.
+ **
+ **   $Id$
+ **
+ ***********************************************************************/
+
+#ifndef QT_5
+#include <QtGui>
+#else
+#include <QtWidgets>
+#endif
+
+#include "calculator.h"
+#include "layout.h"
+#include "logbook.h"
+#include "preflightlogbookspage.h"
+
+#ifdef FLARM
+#include "flarmlogbook.h"
+#endif
+
+PreFlightLogBooksPage::PreFlightLogBooksPage(QWidget *parent) :
+  QWidget(parent)
+{
+  setObjectName("PreFlightLogBooksPage");
+  setWindowFlags( Qt::Tool );
+  setWindowModality( Qt::WindowModal );
+  setAttribute(Qt::WA_DeleteOnClose);
+  setWindowTitle( tr("PreFlight - Logbooks") );
+
+  if( parent )
+    {
+      resize( parent->size() );
+    }
+
+  QHBoxLayout *contentLayout = new QHBoxLayout(this);
+
+  // Top layout's parent is the scroll widget
+  QGridLayout *topLayout = new QGridLayout;
+  contentLayout->addLayout( topLayout );
+
+  int row = 0;
+
+  topLayout->setRowStretch(row, 10);
+  row++;
+
+  QLabel* lbl = new QLabel(tr("My flight book:"));
+  topLayout->addWidget(lbl, row, 0);
+  QPushButton* button = new QPushButton( tr("Open") );
+  topLayout->addWidget(button, row, 1 );
+  row++;
+
+  connect(button, SIGNAL(pressed()), SLOT(slotOpenLogbook()));
+
+#ifdef FLARM
+  topLayout->setRowMinimumHeight(row, 10);
+  row++;
+
+  lbl = new QLabel(tr("Flarm flight book:"));
+  topLayout->addWidget(lbl, row, 0);
+
+  button = new QPushButton( tr("Open") );
+  topLayout->addWidget(button, row, 1 );
+  row++;
+
+  extern Calculator *calculator;
+
+  if( calculator->moving() )
+    {
+      // Disable Flarm flight downloads if we are moving.
+      button->setEnabled( false );
+    }
+  else
+    {
+      connect(button, SIGNAL(pressed()), SLOT(slotOpenFlarmFlights()));
+    }
+
+#endif
+
+  topLayout->setRowStretch(row, 10);
+  topLayout->setColumnStretch(2, 10);
+
+  QPushButton *cancel = new QPushButton(this);
+  cancel->setIcon(QIcon(GeneralConfig::instance()->loadPixmap("cancel.png")));
+  cancel->setIconSize(QSize(IconSize, IconSize));
+  cancel->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::QSizePolicy::Preferred);
+
+  QLabel *titlePix = new QLabel(this);
+  titlePix->setPixmap(GeneralConfig::instance()->loadPixmap("preflight.png"));
+
+  connect(cancel, SIGNAL(pressed()), this, SLOT(slotReject()));
+
+  QVBoxLayout *buttonBox = new QVBoxLayout;
+  buttonBox->setSpacing(0);
+  buttonBox->addStretch(2);
+  buttonBox->addWidget(cancel, 1);
+  buttonBox->addStretch(2);
+  buttonBox->addWidget(titlePix);
+  contentLayout->addLayout(buttonBox);
+}
+
+PreFlightLogBooksPage::~PreFlightLogBooksPage()
+{
+  // qDebug("PreFlightLogBooksPage::~PreFlightLogBooksPage()");
+}
+
+void PreFlightLogBooksPage::slotAccept()
+{
+  emit closingWidget();
+  QWidget::close();
+}
+
+void PreFlightLogBooksPage::slotReject()
+{
+  emit closingWidget();
+  QWidget::close();
+}
+
+void PreFlightLogBooksPage::slotOpenLogbook()
+{
+  Logbook* lbw = new Logbook( this );
+  lbw->setVisible( true );
+}
+
+#ifdef FLARM
+
+/**
+ * Called to open the Flarm flight download dialog.
+ */
+void PreFlightLogBooksPage::slotOpenFlarmFlights()
+{
+  FlarmLogbook* flb = new FlarmLogbook( this );
+  flb->setVisible( true );
+}
+
+#endif
---
+++ cumulus/preflightlogbookspage.h
@@ -0,0 +1,91 @@
+/***********************************************************************
+**
+**   preflightlogbookspage.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2013 Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/**
+ * \class PreFlightLogBooksPage
+ *
+ * \author Axel Pauli
+ *
+ * \brief A widget for pre-flight logbooks management.
+ *
+ * \date 2013
+ *
+ * \version $Id$
+ *
+ */
+
+#ifndef PREFLIGHT_LOGBOOKS_PAGE_H
+#define PREFLIGHT_LOGBOOKS_PAGE_H
+
+#include <QWidget>
+
+class QPushButton;
+
+class PreFlightLogBooksPage : public QWidget
+{
+  Q_OBJECT
+
+ private:
+
+  Q_DISABLE_COPY ( PreFlightLogBooksPage )
+
+ public:
+
+  PreFlightLogBooksPage(QWidget *parent=0);
+
+  virtual ~PreFlightLogBooksPage();
+
+ private slots:
+
+  /**
+   * Called to open the flight logbook dialog.
+   */
+  void slotOpenLogbook();
+
+  /**
+   * Called if the Ok button is pressed.
+   */
+  void slotAccept();
+
+  /**
+   * Called if the Cancel button is pressed.
+   */
+  void slotReject();
+
+#ifdef FLARM
+
+  /**
+   * Called to open the Flarm download flight dialog.
+   */
+  void slotOpenFlarmFlights();
+
+#endif
+
+ signals:
+
+  /**
+   * Emitted, if settings have been changed.
+   */
+  void settingsChanged();
+
+  /**
+   * Emitted, if the widget is closed.
+   */
+  void closingWidget();
+};
+
+#endif
