b050599c0111b945acc698779872fd3f6af8acd5
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2013-12-02 20:04:39 +0000

    Android's barometer sensor made available for Cumulus
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@6416 c8f415cb-fa42-0410-b21e-89df95f64ae5

--- cumulus/android/src/org/kflog/cumulus/CumulusActivity.java
+++ cumulus/android/src/org/kflog/cumulus/CumulusActivity.java
@@ -462,6 +462,7 @@ public class CumulusActivity extends QtActivity
     @Override
     synchronized public void onSensorChanged(SensorEvent event)
       {
+    	// ca. every 200ms a new value is reported.
         // long timestamp = event.timestamp;
         
         // Atmospheric pressure in hPa
@@ -487,8 +488,12 @@ public class CumulusActivity extends QtActivity
         // Calculate altitude according to formula 9, see paragraph 1.1.4.
         // The result is in meters.
         // http://wolkenschnueffler.de/media//DIR_62701/7c9e0b09d2109871ffff8127ac144233.pdf
+        // http://www.deutscher-wetterdienst.de/lexikon/download.php?file=Barometrische_Hoehenformel.pdf
         final double k1 = 29.27 * 288.15;
-        double altitude = k1 * Math.log( 1013.25 / average );
+        final double k2 = 1.09513;
+        final double log_p0_p1 = Math.log( 1013.25 / average );
+
+        double altitude = (k1 * log_p0_p1) / (k2 * log_p0_p1);
         
         // Send altitude value to native application part.
         nativeBaroAltitude( altitude );
