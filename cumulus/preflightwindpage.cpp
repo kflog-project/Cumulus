ad3c26b56820238838bae244487ab44231adda12
axel <axel@c8f415cb-fa42-0410-b21e-89df95f64ae5>
2014-03-24 21:59:36 +0000

    new class used for manual wind enter by the user
    git-svn-id: https://svn.kflog.org/svn/repos/cumulus/qt4/trunk@6597 c8f415cb-fa42-0410-b21e-89df95f64ae5

---
+++ cumulus/preflightwindpage.cpp
@@ -0,0 +1,213 @@
+/***********************************************************************
+**
+**   preflightwindpage.cpp
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2014 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#ifndef QT_5
+#include <QtGui>
+#else
+#include <QtWidgets>
+#endif
+
+#ifdef QTSCROLLER
+#include <QtScroller>
+#endif
+
+#include "generalconfig.h"
+#include "layout.h"
+#include "numberEditor.h"
+#include "preflightwindpage.h"
+#include "rowdelegate.h"
+#include "speed.h"
+
+PreFlightWindPage::PreFlightWindPage( QWidget* parent ) :
+  QWidget( parent )
+{
+  setObjectName("PreFlightWindPage");
+  setWindowFlags( Qt::Tool );
+  setWindowModality( Qt::WindowModal );
+  setAttribute(Qt::WA_DeleteOnClose);
+  setWindowTitle( tr("PreFlight - Wind") );
+
+  if( parent )
+    {
+      resize( parent->size() );
+    }
+
+  QHBoxLayout *contentLayout = new QHBoxLayout;
+  setLayout(contentLayout);
+
+  int msw = QFontMetrics(font()).width("999 Km/h") + 10;
+  int mdw = QFontMetrics(font()).width("999" + QString(Qt::Key_degree)) + 10;
+
+  QVBoxLayout* windLayout = new QVBoxLayout;
+  contentLayout->addLayout( windLayout, 5 );
+
+  windLayout->setSpacing(5);
+  windLayout->setMargin(0);
+
+  QHBoxLayout* editrow = new QHBoxLayout;
+  editrow->setSpacing(5);
+  windLayout->addLayout( editrow );
+  windLayout->addSpacing( 10 );
+
+  m_windCheckBox = new QCheckBox( tr("Manual Wind") );
+  editrow->addWidget(m_windCheckBox);
+  connect( m_windCheckBox, SIGNAL(stateChanged(int)),
+           this, SLOT(slotWindCbStateChanged(int)));
+
+  QLabel* label = new QLabel( tr("WD"), this );
+  editrow->addWidget(label);
+
+  m_windDirection = new NumberEditor( this );
+#ifndef ANDROID
+  m_windDirection->setToolTip( tr("Wind Direction") );
+#endif
+  m_windDirection->setPmVisible(false);
+  m_windDirection->setRange( 0, 360 );
+  m_windDirection->setTip("0...360");
+  m_windDirection->setMaxLength(3);
+  m_windDirection->setValue( GeneralConfig::instance()->getManualWindDirection() );
+  m_windDirection->setSuffix( QString(Qt::Key_degree) );
+  m_windDirection->setMinimumWidth( mdw );
+  editrow->addWidget(m_windDirection);
+
+  label = new QLabel( tr("WS"), this );
+  editrow->addWidget(label);
+
+  m_windSpeed = new NumberEditor( this );
+#ifndef ANDROID
+  m_windSpeed->setToolTip( tr("Wind Speed") );
+#endif
+  m_windSpeed->setPmVisible(false);
+  m_windSpeed->setRange( 0, 999 );
+  m_windSpeed->setMaxLength(3);
+  m_windSpeed->setValue( GeneralConfig::instance()->getManualWindSpeed() );
+  m_windSpeed->setSuffix( " " + Speed::getWindUnitText() );
+  m_windSpeed->setMinimumWidth( msw );
+  editrow->addWidget(m_windSpeed);
+  editrow->addStretch(10);
+
+  //----------------------------------------------------------------------------
+  windLayout->addWidget( new QLabel( tr("Wind Statistics") ) );
+
+  m_windListStatistics = new QTreeWidget;
+
+  m_windListStatistics->setRootIsDecorated(false);
+  m_windListStatistics->setItemsExpandable(false);
+  m_windListStatistics->setUniformRowHeights(true);
+  m_windListStatistics->setAlternatingRowColors(true);
+  m_windListStatistics->setSortingEnabled(false);
+  m_windListStatistics->setColumnCount(3);
+  m_windListStatistics->setFocus();
+  m_windListStatistics->setVerticalScrollMode( QAbstractItemView::ScrollPerPixel );
+  m_windListStatistics->setHorizontalScrollMode( QAbstractItemView::ScrollPerPixel );
+
+#ifdef QSCROLLER
+  QScroller::grabGesture(m_windListStatistics->viewport(), QScroller::LeftMouseButtonGesture);
+#endif
+
+#ifdef QTSCROLLER
+  QtScroller::grabGesture(m_windListStatistics->viewport(), QtScroller::LeftMouseButtonGesture);
+#endif
+
+  // set new row height from configuration
+  int afMargin = GeneralConfig::instance()->getListDisplayAFMargin();
+  m_windListStatistics->setItemDelegate( new RowDelegate( m_windListStatistics, afMargin ) );
+
+  QStringList sl;
+  sl << tr("Arrow")
+     << tr("Direction")
+     << tr("Speed");
+
+  m_windListStatistics->setHeaderLabels(sl);
+
+  QTreeWidgetItem* headerItem = m_windListStatistics->headerItem();
+  headerItem->setTextAlignment( 0, Qt::AlignCenter );
+  headerItem->setTextAlignment( 1, Qt::AlignCenter );
+  headerItem->setTextAlignment( 2, Qt::AlignCenter );
+
+  windLayout->addWidget( m_windListStatistics, 10 );
+
+  QPushButton *cancel = new QPushButton(this);
+  cancel->setIcon(QIcon(GeneralConfig::instance()->loadPixmap("cancel.png")));
+  cancel->setIconSize(QSize(IconSize, IconSize));
+  cancel->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::QSizePolicy::Preferred);
+
+  QPushButton *ok = new QPushButton(this);
+  ok->setIcon(QIcon(GeneralConfig::instance()->loadPixmap("ok.png")));
+  ok->setIconSize(QSize(IconSize, IconSize));
+  ok->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::QSizePolicy::Preferred);
+
+  QLabel *titlePix = new QLabel(this);
+  titlePix->setPixmap(GeneralConfig::instance()->loadPixmap("preflight.png"));
+
+  connect(ok, SIGNAL(pressed()), this, SLOT(slotAccept()));
+  connect(cancel, SIGNAL(pressed()), this, SLOT(slotReject()));
+
+  QVBoxLayout *buttonBox = new QVBoxLayout;
+  buttonBox->setSpacing(0);
+  buttonBox->addStretch(2);
+  buttonBox->addWidget(cancel, 1);
+  buttonBox->addSpacing(30);
+  buttonBox->addWidget(ok, 1);
+  buttonBox->addStretch(2);
+  buttonBox->addWidget(titlePix);
+  contentLayout->addLayout(buttonBox);
+
+  m_windCheckBox->setCheckState( GeneralConfig::instance()->isManualWindEnabled() ? Qt::Checked : Qt::Unchecked );
+  slotWindCbStateChanged( GeneralConfig::instance()->isManualWindEnabled() ? Qt::Checked : Qt::Unchecked );
+}
+
+PreFlightWindPage::~PreFlightWindPage()
+{
+  // qDebug("PreFlightWindPage::~PreFlightWindPage()");
+}
+
+void PreFlightWindPage::showEvent(QShowEvent *)
+{
+  m_windListStatistics->resizeColumnToContents(0);
+  m_windListStatistics->resizeColumnToContents(1);
+  m_windListStatistics->resizeColumnToContents(2);
+}
+
+void PreFlightWindPage::slotWindCbStateChanged( int state )
+{
+  bool enabled = false;
+
+  if( state == Qt::Checked )
+    {
+      enabled = true;
+    }
+  else
+    {
+      enabled = false;
+    }
+
+  m_windDirection->setEnabled( enabled );
+  m_windSpeed->setEnabled( enabled );
+}
+
+void PreFlightWindPage::slotAccept()
+{
+  emit closingWidget();
+  QWidget::close();
+}
+
+void PreFlightWindPage::slotReject()
+{
+  emit closingWidget();
+  QWidget::close();
+}
---
+++ cumulus/preflightwindpage.h
@@ -0,0 +1,103 @@
+/***********************************************************************
+**
+**   preflightwindpage.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2014 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/**
+ * \class PreFlightWindPage
+ *
+ * \author Axel Pauli
+ *
+ * \brief A widget for pre-flight wind settings.
+ *
+ * \date 2014
+ *
+ * \version $Id$
+ *
+ */
+
+#ifndef PRE_FLIGHT_WIND_PAGE_H
+#define PRE_FLIGHT_WIND_PAGE_H
+
+#include <QList>
+#include <QTreeWidget>
+#include <QWidget>
+#include <QCheckBox>
+
+class NumberEditor;
+
+class PreFlightWindPage : public QWidget
+{
+  Q_OBJECT
+
+ private:
+
+  /**
+   * That macro forbids the copy constructor and the assignment operator.
+   */
+  Q_DISABLE_COPY( PreFlightWindPage )
+
+ public:
+
+  PreFlightWindPage( QWidget* parent );
+
+  virtual ~PreFlightWindPage();
+
+ protected:
+
+  void showEvent(QShowEvent *);
+
+ private:
+
+ signals:
+
+   /**
+    * Emitted, if the widget is closed.
+    */
+   void closingWidget();
+
+ private slots:
+
+  /**
+  * Called, if the state of the m_windCheckBox is changed.
+  */
+  void slotWindCbStateChanged( int state );
+
+  /**
+   * Called if the Ok button is pressed.
+   */
+  void slotAccept();
+
+  /**
+   * Called if the Cancel button is pressed.
+   */
+  void slotReject();
+
+ private:
+
+  /** check box for manually wind activation. */
+  QCheckBox* m_windCheckBox;
+
+  /** editor box for wind direction entry*/
+  NumberEditor* m_windDirection;
+
+  /** editor box for wind speed entry */
+  NumberEditor* m_windSpeed;
+
+  /** widget with wind statistics */
+  QTreeWidget* m_windListStatistics;
+};
+
+#endif
