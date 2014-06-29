b5b47836b899a6939ce6a24ad23a439515b39616
Axel Pauli <kflog.cumulus@gmail.com>
2014-06-29 09:57:57 +0200

    preparing of the next release

--- cumulus/preflightwaypointpage.cpp
+++ cumulus/preflightwaypointpage.cpp
@@ -70,12 +70,10 @@ PreFlightWaypointPage::PreFlightWaypointPage(QWidget *parent) :
   sa->setWidget( sw );
 
 #ifdef QSCROLLER
-  sa->viewport()->setVerticalScrollMode( QAbstractItemView::ScrollPerPixel );
   QScroller::grabGesture( sa->viewport(), QScroller::LeftMouseButtonGesture );
 #endif
 
 #ifdef QTSCROLLER
-  sa->viewport()->setVerticalScrollMode( QAbstractItemView::ScrollPerPixel );
   QtScroller::grabGesture( sa->viewport(), QtScroller::LeftMouseButtonGesture );
 #endif
 
@@ -166,12 +164,10 @@ PreFlightWaypointPage::PreFlightWaypointPage(QWidget *parent) :
   m_airfieldBox->view()->setHorizontalScrollMode( QAbstractItemView::ScrollPerPixel );
 
 #ifdef QSCROLLER
-  m_airfieldBox->view()->setVerticalScrollMode( QAbstractItemView::ScrollPerPixel );
   QScroller::grabGesture( m_airfieldBox->view()->viewport(), QScroller::LeftMouseButtonGesture );
 #endif
 
 #ifdef QTSCROLLER
-  m_airfieldBox->view()->setVerticalScrollMode( QAbstractItemView::ScrollPerPixel );
   QtScroller::grabGesture( m_airfieldBox->view()->viewport(), QtScroller::LeftMouseButtonGesture );
 #endif
 
