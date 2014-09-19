7aa2b725622f70cad2cd39ed8c2de3cef64d537a
Axel Pauli <kflog.cumulus@gmail.com>
2014-09-19 16:03:12 +0200

    new class OpenAipLoaderThread created

---
+++ cumulus/openAipLoaderThread.cpp
@@ -0,0 +1,98 @@
+/***********************************************************************
+**
+**   openAipLoaderThread.cpp
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2014 by Axel Pauli <kflog.cumulus@gmail.com>
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#include <csignal>
+
+#include <QtCore>
+
+#include "airfield.h"
+#include "openaipairfieldloader.h"
+#include "openAipLoaderThread.h"
+#include "radiopoint.h"
+
+OpenAipLoaderThread::OpenAipLoaderThread( QObject *parent,
+                                          enum Poi poiSource,
+                                          bool readSource ) :
+  QThread( parent ),
+  m_poiSource(poiSource),
+  m_readSource(readSource)
+{
+  setObjectName( "OpenAipLoaderThread" );
+
+  // Activate self destroy after finish signal has been caught.
+  connect( this, SIGNAL(finished()), this, SLOT(deleteLater()) );
+}
+
+OpenAipLoaderThread::~OpenAipLoaderThread()
+{
+}
+
+void OpenAipLoaderThread::run()
+{
+  sigset_t sigset;
+  sigfillset( &sigset );
+
+  // deactivate all signals in this thread
+  pthread_sigmask( SIG_SETMASK, &sigset, 0 );
+
+  if( m_poiSource == Airfields )
+    {
+      // Check if signal is connected to a slot.
+      if( receivers( SIGNAL( loadedAfList( int, QList<Airfield>* )) ) == 0 )
+	{
+	  qWarning() << "OpenAipLoaderThread: No Slot connection to Signal loadedAfList!";
+	  return;
+	}
+    }
+  else if( m_poiSource == NavAids )
+    {
+      // Check if signal is connected to a slot.
+      if( receivers( SIGNAL( loadedNavAidsList( int, QList<RadioPoint>* )) ) == 0 )
+	{
+	  qWarning() << "OpenAipLoaderThread: No Slot connection to Signal loadedNavAidsList!";
+	  return;
+	}
+    }
+
+  int ok = false;
+
+  if( m_poiSource == Airfields )
+    {
+      QList<Airfield>* poiList = new QList<Airfield>;
+      OpenAipAirfieldLoader oaipl;
+
+      ok = oaipl.load( *poiList, m_readSource );
+
+      /* It is expected that a receiver slot is connected to this signal. The
+       * receiver is responsible to delete the passed lists. Otherwise a big
+       * memory leak will occur.
+       */
+      emit loadedAfList( ok, poiList );
+    }
+  else if( m_poiSource == NavAids )
+    {
+      QList<RadioPoint>* poiList = new QList<RadioPoint>;
+
+      // TODO call loading of NavAid list
+
+      /* It is expected that a receiver slot is connected to this signal. The
+       * receiver is responsible to delete the passed lists. Otherwise a big
+       * memory leak will occur.
+       */
+      emit loadedNavAidsList( ok, poiList );
+    }
+}
---
+++ cumulus/openAipLoaderThread.h
@@ -0,0 +1,98 @@
+/***********************************************************************
+**
+**   openAipLoaderThread.h
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2014 by Axel Pauli <kflog.cumulus@gmail.com>
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#ifndef OpenAip_Loader_Thread_h_
+#define OpenAip_Loader_Thread_h_
+
+#include <QObject>
+#include <QList>
+#include <QString>
+#include <QThread>
+
+#include "singlepoint.h"
+#include "airfield.h"
+#include "radiopoint.h"
+
+/**
+* \class OpenAipLoaderThread
+*
+* \author Axel Pauli
+*
+* \brief Class to read OpenAIP data files in an extra thread.
+*
+* This class can read, parse and filter OpenAIP POI data files and store
+* the content in a binary format. All work is done in an extra thread.
+* The results are returned via the signal \ref loadedList.
+*
+* \date 2014
+*
+* \version $Id$
+*/
+
+class OpenAipLoaderThread : public QThread
+{
+  Q_OBJECT
+
+ public:
+
+  /**
+   * POI Source definitions
+   */
+  enum Poi { Airfields, NavAids };
+
+  OpenAipLoaderThread( QObject *parent,
+                       enum Poi poiSource,
+                       bool readSource=false );
+
+  virtual ~OpenAipLoaderThread();
+
+ protected:
+
+  /**
+   * That is the main method of the thread.
+   */
+  void run();
+
+ signals:
+
+  /**
+  * This signal emits the results of the OpenAIP load. The receiver slot is
+  * responsible to delete the dynamic allocated list in every case.
+  *
+  * \param loadedLists  The number of loaded lists
+  * \param airfieldList The list with the POI data
+  *
+  */
+  void loadedAfList( int loadedLists, QList<Airfield>* airfieldList );
+
+  /**
+  * This signal emits the results of the OpenAIP load. The receiver slot is
+  * responsible to delete the dynamic allocated list in every case.
+  *
+  * \param loadedLists  The number of loaded lists
+  * \param navAidList   The list with the POI data
+  *
+  */
+  void loadedNavAidsList( int loadedLists, QList<RadioPoint>* navAidList );
+
+ private:
+
+  enum Poi m_poiSource;
+  bool m_readSource;
+};
+
+#endif /* OpenAip_Loader_Thread_h_ */
