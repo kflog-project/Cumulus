f72cc93f43790666cbbe1c5f1d348a4d03cc76e8
Axel Pauli <kflog.cumulus@gmail.com>
2014-12-31 14:27:10 +0100

    new method downloadFile( String urlIn, String destinationIn ) added but currently not used due to problems with crashes at the native C++ side.

--- cumulus/android/src/org/kflog/cumulus/CumulusActivity.java
+++ cumulus/android/src/org/kflog/cumulus/CumulusActivity.java
@@ -12,8 +12,6 @@
  **   This file is distributed under the terms of the General Public
  **   License. See the file COPYING for more information.
  **
- **   $Id: CumulusActivity.java 6452 2014-01-02 20:16:42Z axel $
- **
  ***********************************************************************/
 
 package org.kflog.cumulus;
@@ -27,7 +25,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.Object;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
@@ -35,7 +38,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Notification;
@@ -63,6 +66,8 @@ import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.media.AsyncPlayer;
 import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -75,7 +80,6 @@ import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
-
 import org.kde.necessitas.origo.QtActivity;
 import org.kflog.cumulus.BluetoothService;
 import org.kflog.cumulus.CumulusIOIO;
@@ -89,7 +93,7 @@ import org.kflog.cumulus.CumulusIOIO;
  * 
  * @date 2012-2014
  * 
- * @version $Id: CumulusActivity.java 6452 2014-01-02 20:16:42Z axel $
+ * @version 1.0
  * 
  * @short This class handles the Cumulus activity live cycle.
  * 
@@ -439,6 +443,170 @@ public class CumulusActivity extends QtActivity
           }
       }
     });
+  }
+  
+  /**
+   * Definition of Qt Network errors.
+   */
+  static final int NoError = 0;
+  	
+  // network layer errors [relating to the destination server] (1-99):
+  static final int ConnectionRefusedError = 1;
+  static final int RemoteHostClosedError = 2;
+  static final int HostNotFoundError = 3;
+  static final int TimeoutError = 4;
+  static final int OperationCanceledError = 5;
+  static final int SslHandshakeFailedError = 6;
+  static final int TemporaryNetworkFailureError = 7;
+  static final int UnknownNetworkError = 99;
+  	
+  // content errors (201-299):
+  static final int ContentAccessDenied = 201;
+  static final int ContentOperationNotPermittedError = 202;
+  static final int ContentNotFoundError = 203;
+  static final int AuthenticationRequiredError = 204;
+  static final int ContentReSendError = 205;
+  static final int UnknownContentError = 299;
+  	
+  // protocol errors
+  static final int ProtocolUnknownError = 301;
+  static final int ProtocolInvalidOperationError = 302;
+  static final int ProtocolFailure = 399;
+
+  /**
+   * Called from JNI to request a HTTP file download from the native side.
+   * 
+   * @param url Url pointing to the source to be downloaded.
+   * 
+   * @param destination Full file path, where the downloaded file shall be stored.
+   * 
+   * @return Result code of download action.
+   * 
+   */
+  @SuppressLint("SimpleDateFormat")
+  int downloadFile( String urlIn, String destinationIn )
+  {
+	Log.i("DownloadFile", "Entry URL: " + urlIn + ", Dest: " + destinationIn);
+	  
+	if( isNetworkAvailable() == false )
+	{
+		Log.e("DownloadFile", "No network available!");
+		return TemporaryNetworkFailureError;
+	}
+	
+    String strDate = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+    // Create a temporary filename by appending a date-time string
+	String outputFileName = destinationIn + "_" + strDate;
+	
+	BufferedOutputStream outBuffer = null;
+	
+	try
+	{
+		outBuffer = new BufferedOutputStream(new FileOutputStream(outputFileName));
+	} 
+	catch (FileNotFoundException e)
+	{
+		Log.e("DownloadFile", "Cannot open output file: " + e.getMessage());
+		return UnknownNetworkError;
+	}
+	
+	URL url = null;
+	
+	try
+	{
+		url = new URL(urlIn);
+	}
+	catch( MalformedURLException e)
+	{
+		Log.e("DownloadFile", "Malformed URL:" + e.getMessage());
+		try { outBuffer.close();
+		} catch (IOException e1) {}
+		new File(outputFileName).delete();
+		return UnknownNetworkError;
+	}
+	
+	URLConnection urlConnection = null;
+	
+	try
+	{
+		urlConnection = url.openConnection();
+	} catch (IOException e) {
+		Log.e("DownloadFile", "Open URL error:" + e.getMessage());
+		try { outBuffer.close();
+		} catch (IOException e1) {}
+		new File(outputFileName).delete();
+		return UnknownNetworkError;
+	}
+	
+	urlConnection.setReadTimeout(10000);
+	urlConnection.setConnectTimeout(15000);
+	
+	try
+	{
+		urlConnection.setRequestProperty("User-Agent", "Cumulus/5.X (Qt/Android)");
+	}
+	catch (Exception e)
+	{
+		Log.e("DownloadFile", "setRequestProperty error" + e.getMessage());
+		try { outBuffer.close();
+		} catch (IOException e1) {}
+		new File(outputFileName).delete();
+		return UnknownNetworkError;		
+	}
+
+    long total = 0;
+    InputStream input = null;
+    
+	try
+	{
+		// open URL connection
+		urlConnection.connect();
+		
+	    // this will be useful so that you can show a typical 0-100% progress bar
+	    // int fileLength = urlConnection.getContentLength();
+	
+	    // download the file
+	    input = new BufferedInputStream(urlConnection.getInputStream());
+	
+	    byte data[] = new byte[8192];
+	    int count;
+	    
+	    while ((count = input.read(data)) != -1)
+	    {
+	        total += count;
+	        outBuffer.write(data, 0, count);
+	    }
+	
+	    outBuffer.flush();
+	    outBuffer.close();
+	    input.close();
+	}
+	catch (IOException e)
+	{
+		Log.e("DownloadFile", "Read input stream failed:" + e.getMessage());
+		try
+		{
+			input.close();
+			outBuffer.close();
+		}
+		catch (IOException e1) {}
+		
+		new File(outputFileName).delete();
+		return UnknownNetworkError;		
+	}
+
+	// Rename temporary download file
+	new File(outputFileName).renameTo(new File(destinationIn));
+	
+	if( total == 0 )
+	{
+		return ContentNotFoundError;
+	}
+	  
+	Log.i("DownloadFile", "OK: Entry URL: " + urlIn + ", Dest: " + destinationIn);
+
+	return NoError;
   }
 
   /**
@@ -2506,6 +2674,28 @@ public class CumulusActivity extends QtActivity
     return true;
   }
 
+  /**
+   * Checks, if a network connectivity is available.
+   * 
+   * @return true in case of success otherwise false
+   */
+  public boolean isNetworkAvailable()
+  {
+  	ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+  	
+  	NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+  	
+  	// if no network is available networkInfo will be null
+  	// otherwise check if we are connected
+  	if (networkInfo != null &&  networkInfo.isConnected() )
+  	{
+  	  Log.i(TAG, "Network available and connected.");
+  	  return true;
+  	}
+  
+  	return true;
+  }
+  
   private void switchOffScreenDimming()
   {
     // Switch user screen brightness on
