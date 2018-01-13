ebe7c0b6889338d7a6537ce67d5f76a5c37683d1
axel <axel@hugo-pc-linux>
2018-01-13 18:18:44 +0100

    prepare next delivery

--- cumulus/target.h
+++ cumulus/target.h
@@ -3,7 +3,7 @@
              This file is also used during Debian package build
              as release information.
 
-  copyright: (C) 2007-2017 by Axel Pauli
+  copyright: (C) 2007-2018 by Axel Pauli
 
   Email of maintainer: kflog.cumulus@gmail.com
 
@@ -21,6 +21,6 @@
 #ifndef target_h
 #define target_h
 
-#define CU_VERSION "5.31.0"
+#define CU_VERSION "5.31.1"
 
 #endif
