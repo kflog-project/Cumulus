d8e4ab99bcc6f4fb4cceac6f4c2806bfc7f41dfa
Axel Pauli <kflog.cumulus@gmail.com>
2021-04-14 12:50:38 +0200

    spelling error corrected

--- cumulus/locale/de/cumulus_de.ts
+++ cumulus/locale/de/cumulus_de.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de_DE" sourcelanguage="en_US">
+<TS version="2.1" language="de_DE" sourcelanguage="en_US">
 <context>
     <name>AboutWidget</name>
     <message>
@@ -3510,7 +3510,7 @@ USB Stick eingeschoben?</translation>
     <message>
         <location filename="../../preflightwindpage.cpp" line="79"/>
         <source>Manual Wind</source>
-        <translation>Manueller Winf</translation>
+        <translation type="unfinished">Manueller Wind</translation>
     </message>
     <message>
         <location filename="../../preflightwindpage.cpp" line="88"/>
