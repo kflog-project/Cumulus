00518dddd0bc45a65543b5958f1459467135bb0b
Axel Pauli <kflog.cumulus@gmail.com>
2022-01-02 19:54:13 +0100

    year updated

--- cumulus/mainwindow.cpp
+++ cumulus/mainwindow.cpp
@@ -5,7 +5,7 @@
 
  copyright            : (C) 2002-2007 by André Somers
 
-                      : (C) 2007-2021 by Axel Pauli
+                      : (C) 2007-2022 by Axel Pauli
 
  maintainer           : Axel Pauli <kflog.cumulus@gmail.com>
 
@@ -2618,7 +2618,7 @@ void MainWindow::slotVersion()
   aw->setWindowTitle( tr( "About Cumulus") );
   aw->setHeaderIcon( GeneralConfig::instance()->loadPixmap("cumulus-desktop48x48.png") );
 
-  QString header( tr("<html>Cumulus %1, &copy; 2002-2021, The Cumulus-Team</html>").arg( QCoreApplication::applicationVersion() ) );
+  QString header( tr("<html>Cumulus %1, &copy; 2002-2022, The Cumulus-Team</html>").arg( QCoreApplication::applicationVersion() ) );
 
   aw->setHeaderText( header );
 
