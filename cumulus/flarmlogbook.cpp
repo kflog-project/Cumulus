c5494ae0e55590ae01816e6b7312198b6e98fdc4
Axel Pauli <kflog.cumulus@gmail.com>
2022-01-04 23:06:47 +0100

    date updated

--- cumulus/flarmlogbook.cpp
+++ cumulus/flarmlogbook.cpp
@@ -6,7 +6,7 @@
 **
 ************************************************************************
 **
-**   Copyright (c): 2012-2021 Axel Pauli
+**   Copyright (c): 2012-2022 Axel Pauli
 **
 **   This file is distributed under the terms of the General Public
 **   License. See the file COPYING for more information.
