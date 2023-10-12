fb49f2574910801cc4e4078ffc5e66e14f8b3acb
Axel pauli <kflog.cumulus@gmail.com>
2023-10-12 15:07:55 +0200

    new created class for Flarm

---
+++ cumulus/FlarmDb.cpp
@@ -0,0 +1,162 @@
+/***********************************************************************
+**
+**   FlarmDb.cpp
+**
+**   Created on: 12.10.2023 by Axel Pauli
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2023 by Axel Pauli <kflog.cumulus@gmail.com>
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+***********************************************************************/
+
+#include <QtCore>
+
+#include "FlarmDb.h"
+#include "generalconfig.h"
+
+QHash<uint, QString> FlarmDb::m_datamap;
+QMutex FlarmDb::m_mutex;
+
+int FlarmDb::loadData()
+{
+  // Set a global lock during execution to avoid calls in parallel.
+  QMutexLocker locker( &m_mutex );
+  QElapsedTimer t; t.start();
+  uint items = 0; // number of successfully loaded items
+
+  m_datamap.clear();
+
+  // Check, which file the user wants to load.
+  QDir dir( GeneralConfig::instance()->getUserDataDirectory() );
+  QString url = GeneralConfig::instance()->getFlarmDBUrl();
+
+  if( url.isEmpty() )
+    {
+      // No file shall be loaded
+      qWarning() << "FlarmDb: No DB file defined for loading!";
+      return items;
+    }
+
+  QString fname = QFileInfo( url.mid( 6 ) ).fileName();
+  QString fpath = dir.absolutePath() + "/flarmDb/" + fname;
+  QFile file( fpath);
+
+  if( file.open( QIODevice::ReadOnly | QIODevice::Text) == false )
+    {
+      qWarning( "FlarmDb: Can't open DB file %s for writing!"
+                " Aborting ...",
+                fpath.toLatin1().data() );
+
+      return items;
+    }
+
+  QString fs = GeneralConfig::instance()->getFlarmDBFilter().toUpper();
+  QStringList filterList = fs.split( QRegExp("[\\s,]+"), Qt::SkipEmptyParts);
+
+  QTextStream in( &file );
+  in.setCodec( "ISO 8859-15" );
+
+  while( ! in.atEnd() )
+    {
+      QString line = in.readLine();
+
+      if( items == 0 )
+        {
+          // Magic id is expected
+          qDebug() << "Flarm DB magic" << line;
+          items++;
+          continue;
+        }
+
+      if( line.size() < 172 )
+        {
+          // ignore short line.
+          qDebug() << "FlarmDb: line shorter than 172:" << line.size();
+          continue;
+        }
+
+      QByteArray ba = QByteArray::fromHex( line.toLatin1() );
+
+      for( int i=0; i < ba.size(); i++ )
+        {
+          if( ba[i] > (char) 0x7e || ba[i] < ' ' )
+            {
+              ba[i] = '?';
+            }
+        }
+
+      bool ok;
+      uint fid = ba.mid( 0, 6 ).toUInt( &ok, 16);
+      QByteArray fdata = ba.mid( 27, 21 ).trimmed().toUpper(); // KZ
+      QString kz = QString( fdata ).toUpper();
+
+      if( filterList.size() > 0 )
+        {
+          // Check filter for taking
+          bool takeit = false;
+
+          for( int i=0; i < filterList.size(); i++ )
+            {
+              if( kz.startsWith( filterList.at(i) ) == true )
+                {
+                  takeit = true;
+                  break;
+                }
+            }
+
+          if( takeit == false )
+            {
+              continue;
+            }
+        }
+
+      items++;
+      fdata.append( "|" ); // Trenner
+      fdata.append( ba.mid( 48, 21 ).trimmed() ); // Type
+      fdata.append( "|" ); // Trenner
+      fdata.append( ba.mid( 76, 3 ).trimmed() ); // WKZ
+      fdata.append( "|" ); // Trenner
+      fdata.append( ba.mid( 79 ).trimmed() ); // Frequenz
+      m_datamap.insert( fid, fdata );
+    } // End of While
+
+  qDebug( "FlarmDb: %d items loaded in %lldms", items-1, t.elapsed() );
+  return items-1;
+}
+
+bool FlarmDb::getData( int id, QString &data )
+{
+  return false;
+}
+
+/*---------------------- FlarmDbThread --------------------------------*/
+
+#include <csignal>
+
+FlarmDbThread::FlarmDbThread( QObject *parent ) :
+  QThread( parent )
+{
+  setObjectName( "FlarmDbThread" );
+
+  // Activate self destroy after finish signal has been caught.
+  connect( this, SIGNAL(finished()), this, SLOT(deleteLater()) );
+}
+
+FlarmDbThread::~FlarmDbThread()
+{
+}
+
+void FlarmDbThread::run()
+{
+  sigset_t sigset;
+  sigfillset( &sigset );
+
+  // deactivate all signals in this thread
+  pthread_sigmask( SIG_SETMASK, &sigset, 0 );
+}
---
+++ cumulus/FlarmDb.h
@@ -0,0 +1,120 @@
+/***********************************************************************
+**
+**   FlarmDb.h
+**
+**   Created on: 12.10.2023
+**
+**   This file is part of Cumulus.
+**
+************************************************************************
+**
+**   Copyright (c):  2023 by Axel Pauli <kflog.cumulus@gmail.com>
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+***********************************************************************/
+
+/**
+ * \class FlarmDb
+ *
+ * \author Axel Pauli
+ *
+ * \brief Class for reading and accessing special data.
+ *
+ * Class for reading and accessing special data.
+ *
+ * \see https://www.flarmnet.org
+ *
+ * \date 2023
+ *
+ * \version 1.0
+ */
+
+#pragma once
+
+#include <QHash>
+#include <QMutex>
+#include <QString>
+
+class FlarmDb
+{
+  public:
+
+  /**
+   * Constructor
+   */
+  FlarmDb()
+  {
+  };
+
+  /**
+   * Destructor
+   */
+  virtual ~FlarmDb()
+  {
+  };
+
+  /**
+   * Loads data from a file into a hash dictionary.
+   *
+   * @returns The number of successfully loaded items
+   *
+   */
+  static int loadData();
+
+  /**
+   * Read the content of a compiled file and put it into the passed
+   * list.
+   *
+   * @param id identifier key
+   * @param data related data fetched by key.
+   * @return true (success) or false (nothing found)
+   */
+  static bool getData( int id, QString &data );
+
+ private:
+
+  /**
+   * A hash map containing data
+   */
+  static QHash<uint, QString> m_datamap;
+
+  /** Mutex to ensure thread safety. */
+  static QMutex m_mutex;
+};
+
+/******************************************************************************/
+
+#include <QThread>
+
+/**
+* \class FlarmDbThread
+*
+* \author Axel Pauli
+*
+* \brief Class to read a data file in an extra thread.
+*
+* \date 2023
+*
+* \version 1.0
+*/
+
+class FlarmDbThread : public QThread
+{
+  Q_OBJECT
+
+ public:
+
+  FlarmDbThread( QObject *parent=0 );
+
+  virtual ~FlarmDbThread();
+
+ protected:
+
+  /**
+   * That is the main method of the thread.
+   */
+  void run();
+};
+
