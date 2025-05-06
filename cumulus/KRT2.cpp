395eb3be26b6ff8396d6a15b915360f0643886a0
Axel pauli <kflog.cumulus@gmail.com>
2025-05-06 18:15:48 +0200

    include added

--- cumulus/KRT2.cpp
+++ cumulus/KRT2.cpp
@@ -14,6 +14,7 @@
  **   (at your option) any later version.
  **
  ***********************************************************************/
+#include <unistd.h>
 #include <QtCore>
 
 #include "generalconfig.h"
